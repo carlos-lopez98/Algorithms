0c38c721d2d8cc3769342c620a9a305df1efa598
Carlos <cjimenez9811@gmail.com>
2023-09-08 21:08:50 -0700

    Added URLify

---
+++ src/main/java/org/example/URLify.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/*
+* Write a method to replace all spaces in a string with "%20"
+* --see page 90--
+* */
+public class URLify {
+
+
+
+    public static String algoOne(String string){
+        return "";
+    }
+}
