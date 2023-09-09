f979c37ee2c3015df1619070cbc6e36c5acf1b00
Carlos <cjimenez9811@gmail.com>
2023-09-08 20:56:18 -0700

    Check Permutation implemented

--- src/main/java/org/example/CheckPermutation.java
+++ src/main/java/org/example/CheckPermutation.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.*;
+
+/*
+* Given two strings write a method to decide if one is a permutation of the other
+*
+* This means one string must have all characters in the other string
+* */
 public class CheckPermutation {
+
+ public static boolean algoOne(String stringOne, String stringTwo){
+     if(stringOne.length() != stringTwo.length()){
+         return false;
+     }
+
+     Set<char> characterMap = new HashSet<char>();
+
+     for(char current: stringOne.toCharArray()){
+         characterMap.add(current);
+     }
+
+     for(int i = 0; i < stringTwo.length(); i++){
+         if(!characterMap.contains(stringTwo.charAt(i))){
+             return false;
+         }
+     }
+     return true;
+ }
 }
