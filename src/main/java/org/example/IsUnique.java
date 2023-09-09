ffd90ecdd1f76f7f26c22555bd355d2d5c949b95
Carlos <cjimenez9811@gmail.com>
2023-09-08 20:45:04 -0700

    Added second algorithm for IsUnique

--- src/main/java/org/example/IsUnique.java
+++ src/main/java/org/example/IsUnique.java
@@ -11,6 +11,7 @@ import java.util.Set;
 
 public class IsUnique {
 
+    //This one works but uses a data structure
     public static boolean algoOne(String string){
        Set<Character> uniqueSet = new HashSet();
 
@@ -24,4 +25,24 @@ public class IsUnique {
             return false;
         }
     }
+
+    //This one works but doesn't check for things like spaces etc...
+    public static boolean algoTwo(String string){
+
+        String lowerCase = string.toLowerCase();
+
+        for(int i = 0; i < lowerCase.length(); i++){
+            char first = lowerCase.charAt(i);
+            for(int j = i + 1; j < lowerCase.length(); j++){
+                char second = lowerCase.charAt(j);
+                if(first == second){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    //Another possible solution is to use the ASCII code of the string -- **NEED TO IMPLEMENT**
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String test = "Haryjkl";
 
+        System.out.println(IsUnique.algoTwo(test));
     }
 }
