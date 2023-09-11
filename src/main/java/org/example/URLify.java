be780f67918175f8f6d4da6cbf3e79db48d27485
Carlos <cjimenez9811@gmail.com>
2023-09-10 18:58:31 -0700

    Working on URLify

--- src/main/java/org/example/URLify.java
+++ src/main/java/org/example/URLify.java
@@ -2,13 +2,19 @@ package org.example;
 
 /*
 * Write a method to replace all spaces in a string with "%20"
+* The input string will be a character array - in order to be able to do this in place
 * --see page 90--
+*
+* Input: String[] with additional space characters to accomodate the %20 needed
+*           True length of the string "I am dog   ", 8 (True length till end of Dog)
 * */
 public class URLify {
 
+    public static String algoOne(char[] args, int length){
 
 
-    public static String algoOne(String string){
+
+
         return "";
     }
 }
