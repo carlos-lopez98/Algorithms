7c470b8a6606096da2282be386d509be6d3655f3
Carlos <cjimenez9811@gmail.com>
2023-09-15 09:26:29 -0700

    Practicing Matrices

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,27 @@ public class Main {
 //        System.out.println(StringCompression.algoOne("Carlos"));
 //        System.out.println(StringCompression.algoOne("AABBBCC"));
 
-        MultiDimensionalArrays.printIndicesByColumn();
+//        int[][] matrix = {{1,2,3},
+//                          {4,5,6},
+//                          {7,8,9}};
+//
+//        //Excersises Accessing Elements
+//        //2nd Column third row
+//        System.out.println(matrix[2][1]); //8
+//        //3rd Column second row
+//        System.out.println(matrix[1][2]); //6
+//        //Last row, second column
+//        System.out.println(matrix[2][1]); //8
+//        //Last row, third column
+//        System.out.println(matrix[2][2]); //9
+//        //First Column, First Row
+//        System.out.println(matrix[0][0]); //1
+//        //First Column, Second Row
+//        System.out.println(matrix[1][0]); //4
+//
+//        MultiDimensionalArrays.printIndicesByColumn();
+
+
+
     }
 }
