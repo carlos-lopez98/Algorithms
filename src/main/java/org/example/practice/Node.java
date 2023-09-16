dfb65a6c70aed6b2c72158d476915e4f57e181f9
Carlos <cjimenez9811@gmail.com>
2023-09-16 11:50:16 -0700

    DeleteNode implemented

--- src/main/java/org/example/practice/Node.java
+++ src/main/java/org/example/practice/Node.java
@@ -16,4 +16,30 @@ public class Node {
         }
         n.next= end;
     }
+
+    //This method is meant to delete a node that holds specific data
+    //Input: Head node reference to a linkedlist, and data to delete
+    public Node deteteNode(Node head, int data){
+
+        if(head == null) return null;
+
+        Node n = head;
+
+        //Returns a new head reference, effectively deleting it
+        //Since there will be no future references to it, the garbage collector will eventually remove it from memory
+        if(n.data == data){
+            return head.next;
+        }
+
+        while(n.next != null){
+            //You're technically not deleting the nodes
+            //We're just removing it's link in the list, which in turn will be cleaned up by the garbage collector
+         if(n.next.data == data){
+             n.next = n.next.next;
+             return head;
+         }
+          n = n.next;
+        }
+        return head;
+    }
 }
