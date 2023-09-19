c505cc1d5e1f3b2f19cb672f69b460d6bf08ef3a
Carlos <cjimenez9811@gmail.com>
2023-09-19 07:33:17 -0700

    Palindrome LinkedList

--- src/main/java/org/example/PalindromeLinkedList.java
+++ src/main/java/org/example/PalindromeLinkedList.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.practice.Node;
+
 /*
 *
 * Implement a function to check if a linked list is a palindrome
@@ -7,9 +9,21 @@ package org.example;
 *
 * */
 public class PalindromeLinkedList {
+    //A palindrome will be the same forwards and backwards
+    //If a palindrome is a palindrome, then that means we'd only have to check half of the list
+    //As the other half would = the first half
 
 
 
+    public Node reverse(Node node){
+        Node head = null;
 
-
+        while (node!= null){
+            Node n = new Node(node.data);
+            n.next = head;
+            head = n;
+            node = node.next;
+        }
+        return head;
+    }
 }
--- src/main/java/org/example/SumLists.java
+++ src/main/java/org/example/SumLists.java
@@ -1,4 +1,52 @@
 package org.example;
 
+import org.example.practice.Node;
+
+/*
+*
+* You have two numbers represented by a linked list, where each node contains a single digit.
+* The digits are stored in reverse order, such that the 1's digit is at the head of the list.
+* Write a function that adds the two numbers and returns the sum as a linked list.
+*
+* (You can't just convert the linked list into an integer)
+*
+*
+* */
 public class SumLists {
+
+
+    public Node sum(Node integerOne, Node integerTwo){
+        Node sum = null;
+        int carry = 0;
+
+        //Iteration will keep going as long as lists have a value to keep going or carry has a value to add
+        while (integerOne != null || integerTwo != null || carry != 0){
+            Node oneCurr = integerOne;
+            Node twoCurr = integerTwo;
+
+            int sumation = oneCurr.data + twoCurr.data;
+
+            if(sumation >= 10){
+                carry += 1;
+                sumation = Math.abs(sumation - 10);
+            }
+
+            if(sum == null){
+                sum = new Node(sumation);
+                sum.next = new Node(0);
+                sum = sum.next;
+            }else{
+                sum.data = sumation;
+                sum = sum.next;
+            }
+
+
+            integerOne = integerOne.next;
+            integerTwo = integerTwo.next;
+        }
+
+        sum.data += carry;
+
+        return sum;
+    }
 }
