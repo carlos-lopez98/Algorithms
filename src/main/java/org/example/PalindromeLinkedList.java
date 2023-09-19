d14bab858deea4216b477776c4c8f46c006bba0e
Carlos <cjimenez9811@gmail.com>
2023-09-19 07:36:59 -0700

    Explanation on how to reverse a linkedList

--- src/main/java/org/example/PalindromeLinkedList.java
+++ src/main/java/org/example/PalindromeLinkedList.java
@@ -14,14 +14,16 @@ public class PalindromeLinkedList {
     //As the other half would = the first half
 
 
-
     public Node reverse(Node node){
         Node head = null;
 
         while (node!= null){
             Node n = new Node(node.data);
+            //First node's next will become null
             n.next = head;
+            //Then head is moved to the next node
             head = n;
+            //Finally we move the node on original list for the next iteration
             node = node.next;
         }
         return head;
