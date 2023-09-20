ac182b4fcbe528d7390dc95940e685605892d72a
Carlos <cjimenez9811@gmail.com>
2023-09-19 19:29:00 -0700

    Loop Detection Comments Added

--- src/main/java/org/example/LoopDetectionLinkedList.java
+++ src/main/java/org/example/LoopDetectionLinkedList.java
@@ -8,6 +8,13 @@ package org.example;
 * */
 public class LoopDetectionLinkedList {
 
+    //! You can solve this use hashing - loop through your original list
+    // Add the nodes to hashmap as you go using their addresses as the hashvalue
+    //If you run into the same value then there is a cycle, and that value is the new start
+    //of a loop
 
+
+
+    //Second way is to use the tortaise and hare algorithm
 
 }
