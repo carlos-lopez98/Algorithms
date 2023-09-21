f21bcc15b5665f7b981fe7f1aa17be2b1a2080bb
Carlos <cjimenez9811@gmail.com>
2023-09-21 07:29:59 -0700

    Leaders In Array Implementation

--- src/main/java/org/example/LeadersInArray.java
+++ src/main/java/org/example/LeadersInArray.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 *
 * Write a program to print all the leaders in the array. An element is a leader if it is greater than all the elements
@@ -17,4 +20,29 @@ public class LeadersInArray {
     //If another value is higher than current highest, then that one is leader so add to list
     //Then print list in reverse order, as the order will be 2, 5, 17
     //So you want 17, 5, 2 or use stack as it's LIFO
+
+    public static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        int max = 0;
+
+        for(int i = n-1; i >= 0; i--){
+
+            if(arr[i] > max){
+                ints.add(arr[i]);
+                max = arr[i];
+            }
+        }
+
+        //I would have to reverse the list to get the correct output
+        ArrayList<Integer> reversed = new ArrayList<Integer>();
+
+        for(int i = ints.size() - 1; i >= 0; i--){
+            int currentRight = ints.get(i);
+            reversed.add(currentRight);
+        }
+
+        return reversed;
+    }
+
 }
