a27adf7ea58de08c7e9f8f4f5cc3a7a2b4a65532
Carlos <cjimenez9811@gmail.com>
2023-09-22 06:54:21 -0700

    Recursively reverse a string

--- src/main/java/org/example/ReverseAString.java
+++ src/main/java/org/example/ReverseAString.java
@@ -5,7 +5,7 @@ package org.example;
 * */
 public class ReverseAString {
     //You can do this by turning array to string then just rearranging the elements one by one
-
+    //Time complexity(O(n)) space o(1) you need a single array to hold elements in place
     public static String reversed(String string){
         char[] reversed = string.toCharArray();
 
@@ -21,4 +21,19 @@ public class ReverseAString {
         return String.valueOf(reversed);
     }
 
+    //How would I use recursion to reverse a string
+    public static String reversedRecursion(String string){
+        if(string == null || string.length() <= 1){
+            return string;
+        }
+
+        //This is going to reduce the strings size all the way to 1 character
+        /*If you recurse like this, you'll effectively rebuild the string in the original order*/
+        //String reversed = reversedRecursion(string.substring(0, string.length() - 1));
+
+
+        //Here you iterate through the string removing the front character from the front, but adding it to the back
+        //At the end, you'll have a string = "", which will get moved to the back, and complete your reversed string
+        return reversedRecursion(string.substring(1)) + string.charAt(0);
+    }
 }
