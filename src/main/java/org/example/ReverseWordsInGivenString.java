317ee32748527afcc91fca1920e9497b58f07d4e
Carlos <cjimenez9811@gmail.com>
2023-09-22 07:48:13 -0700

    Reverse a String, used split method

--- src/main/java/org/example/ReverseWordsInGivenString.java
+++ src/main/java/org/example/ReverseWordsInGivenString.java
@@ -12,5 +12,26 @@ package org.example;
 */
 public class ReverseWordsInGivenString {
 
+    public static String reverseStringNotWords(String s){
 
+        if(s.isBlank() || s.isEmpty() || s.length() == 1){
+            return s;
+        }
+
+        //Split the string at the dots - it'll return a string array
+        //The "." has a special meaning when splitting strings - it means split all characters - so this returns an empty array
+        String[] words = s.split("\\.");
+        String reversed = new String();
+
+        for(int i = words.length - 1; i > -1; i--){
+
+            if (reversed.isBlank()){
+                reversed += words[i];
+            }else{
+                reversed += "." + words[i];
+            }
+        }
+
+        return reversed;
+    }
 }
