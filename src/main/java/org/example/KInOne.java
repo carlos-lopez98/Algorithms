3c5072f059de77d15ed6c733c5de6aae43904120
Carlos <cjimenez9811@gmail.com>
2023-09-23 20:02:41 -0700

    Three In One Working

--- src/main/java/org/example/KInOne.java
+++ src/main/java/org/example/KInOne.java
@@ -13,14 +13,26 @@ public class KInOne {
 
     //Let's say you want an array of four stacks with each having a capacity of four stackable elements
     public static int[] completeStackArray;
-
-    public static void createStackArray(int stackCapacity, int stacksNeeded){
+    private int stackCapacity;
+    public void createStackArray(int stackCapacity, int stacksNeeded){
         // This will give us an array, that can hold our capacity per stack + the total number of stacks we need
+        this.stackCapacity = stackCapacity;
         completeStackArray = new int[stackCapacity * stacksNeeded];
     }
 
     //This method will push a value, into the stack at the specified position in our completeArray
     public static void pushToKStack(int stackPosition, int value){
+    //First in order to push to the specified stack, we must have space left to push onto it
+        //If our stackPosition has values = to our stackCapacity - throw an error and not add
+        if(stackIsFull){
+            throw new RuntimeException("Specified Stack Is Full");
+        }
+
+    }
+
+    //This will check if the stack being asked for is full -- since our array is split into stacks
+    //Each segment represents a position of a stack
+    private boolean stackIsFull(int stackPosition){
 
 
 
