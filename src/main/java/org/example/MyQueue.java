0437940617058495e1b44470ec404e52241d3796
Carlos <cjimenez9811@gmail.com>
2023-09-25 08:19:25 -0700

    MyQueue Working

--- src/main/java/org/example/MyQueue.java
+++ src/main/java/org/example/MyQueue.java
@@ -2,12 +2,15 @@ package org.example;
 
 /*
 *
-*   Implement a MyQueue class which implements a queue using two stacks
+* Implement a MyQueue class which implements a queue using two stacks
 *
 * */
 public class MyQueue {
 
-
+    //A Queue is FIFO order, something added to the list goes to the end
+    //Something removed from the list -- will be the first element in line
+    //Stacks are LIFO, something added recently will be the first to pop() off the stack
+    //How can I implement the list using a stack?
 
 
 }
