5b32f74a55d793f3084e93aea1a8581fbce33564
Carlos <cjimenez9811@gmail.com>
2023-09-25 20:15:44 -0700

    Working on SortStack Algo

---
+++ src/main/java/org/example/SortStack.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/*
+* Write a program to sort a stack such that the smallest items are on top. You can usean additional temporary stack,
+* but you may not copy the elements into any other data structure (such as an array)
+* The stack supports the following operations: push, pop, peek and isEmpty()
+* */
+public class SortStack {
+}
