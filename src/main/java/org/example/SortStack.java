1b321afe2de8d47f3262b436567b4f63f238c95a
Carlos <cjimenez9811@gmail.com>
2023-09-25 20:41:08 -0700

    SortStack Working

--- src/main/java/org/example/SortStack.java
+++ src/main/java/org/example/SortStack.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import java.util.Stack;
+
 /*
-* Write a program to sort a stack such that the smallest items are on top. You can usean additional temporary stack,
+* Write a program to sort a stack such that the smallest items are on top. You can use an additional temporary stack,
 * but you may not copy the elements into any other data structure (such as an array)
 * The stack supports the following operations: push, pop, peek and isEmpty()
 * */
 public class SortStack {
+
+
+    //Since this algorithm is being done in place no need to return anything
+
+    public void algoOne(Stack<Integer> unsorted){
+        //Temporary Stack used for Sorting
+        Stack<Integer> tempStack = new Stack<>();
+
+        //Use a while loop because we want to go through all elements to make sure they get sorted
+        while(!unsorted.isEmpty()){
+
+
+
+
+        }
+    }
 }
