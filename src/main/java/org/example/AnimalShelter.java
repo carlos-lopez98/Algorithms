c39c2f29151d6254acf713029c9052042840cb4a
Carlos <cjimenez9811@gmail.com>
2023-09-26 07:40:17 -0700

    Animal Shelter Commented Explanation

--- src/main/java/org/example/AnimalShelter.java
+++ src/main/java/org/example/AnimalShelter.java
@@ -11,8 +11,10 @@ package org.example;
 * */
 public class AnimalShelter {
 
-
-
-
+    //Best way to do this, is to simply have two queues/linkedlists to store the animals
+    //One for dogs and one for cats, since user can't select a specific animal we're only going to reference the front/heads of the lists
+    //When dequeueing a dog, we'll pull from the dog structure and vice versa
+    //When dequeueingAny, we'll peek into each list, and return whichever is older
+    //When queueing we'll see what is being passed in, then add to the respective list
 
 }
