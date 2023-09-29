17c730bcde7e6245ee3f94c5a15024c10e2722e3
Carlos <cjimenez9811@gmail.com>
2023-09-28 19:40:58 -0700

    Working on Trees

--- src/main/java/org/example/trees/TreeNode.java
+++ src/main/java/org/example/trees/TreeNode.java
@@ -5,6 +5,7 @@ package org.example.trees;
 * Node used for our trees
 * */
 public class TreeNode {
+    //Notice how the tree nodes have children to relate to the other nodes
 
     public String name;
     public TreeNode[] children;
