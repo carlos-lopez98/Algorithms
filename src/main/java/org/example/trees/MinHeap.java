208edd9aabdfdb7d89b29a80a132698de54b7922
Carlos <cjimenez9811@gmail.com>
2023-09-29 10:30:31 -0700

    Working on MinHeap

--- src/main/java/org/example/trees/MinHeap.java
+++ src/main/java/org/example/trees/MinHeap.java
@@ -30,16 +30,18 @@ public class MinHeap {
                 int parentInd = (index - 1)/2; // Why does this give us the parent index?
                 return parentInd;
         }
-
         private int getLeftChildIndex(int index){
                 int leftChildInd = (index * 2) + 1;
                 return leftChildInd;
         }
-
         //Think about positioning in an array - to deduce why this works
         private int getRightChildIndex(int index){
                 int rightChildIndex = (index * 2) + 2;
                 return rightChildIndex;
         }
+
+        //When adding an element to the heap - it's preffered to add it to the end of the array, then bubble up to sort
+        //This is because, if you were to swap with the root, then bubble down, it would be a lot more complex to implement
+        //Even though the runtimes are not much different
 
 }
