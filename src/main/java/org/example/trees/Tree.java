367d7ab1fa9773a247d579932993bf2b429f1a96
Carlos <cjimenez9811@gmail.com>
2023-09-28 19:42:57 -0700

    Working on Trees

---
+++ src/main/java/org/example/trees/Tree.java
@@ -0,0 +1,19 @@
+package org.example.trees;
+
+
+/*
+*
+* This class is meant to be the start of our tree
+* We only hold a reference to the root node
+*
+* */
+public class Tree {
+    public TreeNode root;
+
+
+    public Tree(){
+    }
+    public Tree(TreeNode root){
+        this.root = root;
+    }
+}
