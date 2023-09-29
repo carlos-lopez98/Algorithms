872b193998d41716523d6ef0fd14f9d21adaa688
Carlos <cjimenez9811@gmail.com>
2023-09-29 16:03:40 -0700

    Two Sum Implemented

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -1,6 +1,9 @@
 package org.example;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 *
 * Given an array of integers nums and an integer target, return indices of the
@@ -11,4 +14,28 @@ package org.example;
 *
 * */
 public class TwoSum {
+
+
+    //This one works by putting all the compliment values into the HashMap
+    //Then iterates through the array, if the array contains the compliment value and
+    //it's not equal to it's same position then return true
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int compliment = target - nums[i];
+            map.put(compliment, i);
+        }
+
+        for(int j = 0; j < nums.length; j++){
+            if(map.containsKey(nums[j]) && map.get(nums[j]) != j){
+                int complimentIndex = map.get(nums[j]);
+
+                return new int[]{j, complimentIndex};
+            }
+        }
+
+        return new int[2];
+    }
 }
