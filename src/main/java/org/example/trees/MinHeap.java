b6316754954d7b6f6ccb1c6930b9151a94200ee3
Carlos <cjimenez9811@gmail.com>
2023-09-29 09:41:33 -0700

    Working on MinHeap

---
+++ src/main/java/org/example/trees/MinHeap.java
@@ -0,0 +1,15 @@
+package org.example.trees;
+
+/*
+*
+* A min heap is an unordered complete binary tree, where each node can have at max two child elements
+* It keeps elements ordered in descending value from root - children, so the root is always the minimum value
+*
+* A max heap would just be the vice versa of this
+*
+* */
+public class MinHeap {
+
+
+
+}
