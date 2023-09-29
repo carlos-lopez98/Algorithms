faccce424548cbd81800019f5124201aa355d0bf
Carlos <cjimenez9811@gmail.com>
2023-09-29 14:20:55 -0700

    Is Palindrome solution explained

---
+++ src/main/java/org/example/IsPalindrome.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+/*
+*
+* Check if a given string is a palindrome
+*
+* */
+public class IsPalindrome {
+
+    //There are two ways to do this, with a stack
+    //Or with two pointers
+
+}
