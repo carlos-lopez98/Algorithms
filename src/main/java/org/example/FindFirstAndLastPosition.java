d918bd01f819cbf4727dfb5d3779751f83855a3b
Carlos <cjimenez9811@gmail.com>
2023-09-29 13:34:43 -0700

    Working with Binary Trees

---
+++ src/main/java/org/example/FindFirstAndLastPosition.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+/*
+*
+* Given an array of integers nums sorted in ascending order, find the starting and ending position
+* of a given target value
+*
+* If target is not found in the array, return [-1, -1]
+*
+* */
+
+public class FindFirstAndLastPosition {
+
+
+
+
+
+}
