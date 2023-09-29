6211d5d02aee5ef674df18a53de82b1b324afe9f
Carlos <cjimenez9811@gmail.com>
2023-09-29 14:01:17 -0700

    Working with Binary Trees

--- src/main/java/org/example/FindFirstAndLastPosition.java
+++ src/main/java/org/example/FindFirstAndLastPosition.java
@@ -14,6 +14,16 @@ public class FindFirstAndLastPosition {
 
 
 
+    //To acheive O(log n) you need to do binary search
+    //This will get you to the target element
+    //But how do you get the first occurrence, you'll run binary search on the left side of the target
+    //Then you'll run the search again, but this time on the right side of the target
+    public int[] indexes(long[] v, long x ){
 
+
+
+
+
+    }
 
 }
