ae91b39110a70e4e60be87c608dbcaa36c88d433
Carlos <cjimenez9811@gmail.com>
2023-09-29 10:15:36 -0700

    Working on MinHeap

--- src/main/java/org/example/trees/MinHeap.java
+++ src/main/java/org/example/trees/MinHeap.java
@@ -10,6 +10,17 @@ package org.example.trees;
 * */
 public class MinHeap {
 
+        // Our min heap needs to have basic functionality
+        //Add, Delete, Peek
+        //When you add/ remove an element you need to be able to resort your heap
+        //You can add helper methods, heapifyDown() or heapifyUp()
+        int size;
+        int[] items;
 
+        //Initializes our heap, to have an array size of the provided size
+        public MinHeap(int size){
+                this.size = size;
+                items = new int[size];
+        }
 
 }
