57bd3b34cb24d3e18f91c156e1414ad076acc88c
Carlos <cjimenez9811@gmail.com>
2023-09-29 15:31:54 -0700

    Two Sum Working

---
+++ src/main/java/org/example/SumOfThree.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/*
+*
+* Given an array of integers, nums, and an integer value, target, determine if there are any three integers
+* in nums whose sum is equal to the target, that is, nums[i] + nums[j] + nums[k] == target.
+* Return TRUE if three such integers exist in the array. Otherwise, return FALSE.
+*
+* */
+
+public class SumOfThree {
+
+
+
+
+
+
+
+
+}
