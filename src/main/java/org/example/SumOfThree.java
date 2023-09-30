82db3c5ff069491e8338798e1badf22ad6a22c97
Carlos <cjimenez9811@gmail.com>
2023-09-30 09:01:45 -0700

    TwoSum Solution Implemented

--- src/main/java/org/example/SumOfThree.java
+++ src/main/java/org/example/SumOfThree.java
@@ -10,15 +10,30 @@ package org.example;
 
 public class SumOfThree {
 
-    //For this one you don't need to return the indexes of the positions that acheive three sum
-    //That means we can sort the array, and just use a three pointer method -- and or
-    //Use our previous twoSum solution -- to get the two sum
-    //While we have a pointer at the beginning of the list -- that will be added to that twoSum
-    //And if that = target then we return true
+    //For this one, think how we don't have to return indices, just true or false
+    //For this one, we can sort the array based of of that giving us O(n log n) runtime to sort
+    //After that you can do the three pointer method, where you keep one pointer at the start
+    //Then based off of your target value you move the inner left pointer or the outer right one
+
+
+    public boolean findThreeSum(int[] nums, int target){
+
+        //Think about the end of your first pointer, we don't need to go to nums.length - 1 or nums.length -2, because
+        //We have three pointers and if we were to go all the way through the array
+        //The first pointer would stop at nums.length -3, second one would stop at nums.length -2 and third at nums.length -1
+     for(int i = 0; i < nums.length - 2; i++){
+         int j = i+1;
+         int k = nums.length -1;
+
+        //You could potentially use a for loop here,but while loop is more readible and concise
+        while(j < k){
 
 
 
 
+         }
+     }
+    }
 
 
 }
