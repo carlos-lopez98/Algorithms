90952602220f41417d39e1d84ea7580552cde35a
Carlos <cjimenez9811@gmail.com>
2023-09-30 09:06:59 -0700

    Practice SumLists

--- src/main/java/org/example/SumLists.java
+++ src/main/java/org/example/SumLists.java
@@ -15,38 +15,42 @@ import org.example.practice.Node;
 public class SumLists {
 
 
-    public Node sum(Node integerOne, Node integerTwo){
-        Node sum = null;
-        int carry = 0;
 
-        //Iteration will keep going as long as lists have a value to keep going or carry has a value to add
-        while (integerOne != null || integerTwo != null || carry != 0){
-            Node oneCurr = integerOne;
-            Node twoCurr = integerTwo;
-
-            int sumation = oneCurr.data + twoCurr.data;
-
-            if(sumation >= 10){
-                carry += 1;
-                sumation = Math.abs(sumation - 10);
-            }
-
-            if(sum == null){
-                sum = new Node(sumation);
-                sum.next = new Node(0);
-                sum = sum.next;
-            }else{
-                sum.data = sumation;
-                sum = sum.next;
-            }
-
-
-            integerOne = integerOne.next;
-            integerTwo = integerTwo.next;
-        }
-
-        sum.data += carry;
-
-        return sum;
-    }
 }
+
+
+
+//    public Node sum(Node integerOne, Node integerTwo){
+//        Node sum = null;
+//        int carry = 0;
+//
+//        //Iteration will keep going as long as lists have a value to keep going or carry has a value to add
+//        while (integerOne != null || integerTwo != null || carry != 0){
+//            Node oneCurr = integerOne;
+//            Node twoCurr = integerTwo;
+//
+//            int sumation = oneCurr.data + twoCurr.data;
+//
+//            if(sumation >= 10){
+//                carry += 1;
+//                sumation = Math.abs(sumation - 10);
+//            }
+//
+//            if(sum == null){
+//                sum = new Node(sumation);
+//                sum.next = new Node(0);
+//                sum = sum.next;
+//            }else{
+//                sum.data = sumation;
+//                sum = sum.next;
+//            }
+//
+//
+//            integerOne = integerOne.next;
+//            integerTwo = integerTwo.next;
+//        }
+//
+//        sum.data += carry;
+//
+//        return sum;
+//    }
--- src/main/java/org/example/SumOfThree.java
+++ src/main/java/org/example/SumOfThree.java
@@ -15,25 +15,28 @@ public class SumOfThree {
     //After that you can do the three pointer method, where you keep one pointer at the start
     //Then based off of your target value you move the inner left pointer or the outer right one
 
-
     public boolean findThreeSum(int[] nums, int target){
-
         //Think about the end of your first pointer, we don't need to go to nums.length - 1 or nums.length -2, because
         //We have three pointers and if we were to go all the way through the array
         //The first pointer would stop at nums.length -3, second one would stop at nums.length -2 and third at nums.length -1
      for(int i = 0; i < nums.length - 2; i++){
+         //You could rename the ints for readibality - left & right or low and high
          int j = i+1;
          int k = nums.length -1;
-
         //You could potentially use a for loop here,but while loop is more readible and concise
         while(j < k){
+            int sum = nums[i] + nums[j] + nums[k];
 
-
-
-
+            if(sum == target){
+                return true;
+            }else if(sum < target){
+                j++;
+            }else{
+                k--;
+            }
          }
+
      }
+     return false;
     }
-
-
 }
