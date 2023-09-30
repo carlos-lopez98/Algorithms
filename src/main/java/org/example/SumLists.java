867ff67f90aea796801c2ecd7ceea4ebb7c8509c
Carlos <cjimenez9811@gmail.com>
2023-09-30 09:36:45 -0700

    Practice SumLists

--- src/main/java/org/example/SumLists.java
+++ src/main/java/org/example/SumLists.java
@@ -10,47 +10,17 @@ import org.example.practice.Node;
 *
 * (You can't just convert the linked list into an integer)
 *
-*
+* Input: 7 -> 1 -> 6 + 5 -> 9 -> 2 = 2 -> 1 > 9
+* Output: 912
 * */
 public class SumLists {
+
+public Node sum(Node integerOne, Node IntegerTwo){
+
+
 
 
 
 }
 
-
-
-//    public Node sum(Node integerOne, Node integerTwo){
-//        Node sum = null;
-//        int carry = 0;
-//
-//        //Iteration will keep going as long as lists have a value to keep going or carry has a value to add
-//        while (integerOne != null || integerTwo != null || carry != 0){
-//            Node oneCurr = integerOne;
-//            Node twoCurr = integerTwo;
-//
-//            int sumation = oneCurr.data + twoCurr.data;
-//
-//            if(sumation >= 10){
-//                carry += 1;
-//                sumation = Math.abs(sumation - 10);
-//            }
-//
-//            if(sum == null){
-//                sum = new Node(sumation);
-//                sum.next = new Node(0);
-//                sum = sum.next;
-//            }else{
-//                sum.data = sumation;
-//                sum = sum.next;
-//            }
-//
-//
-//            integerOne = integerOne.next;
-//            integerTwo = integerTwo.next;
-//        }
-//
-//        sum.data += carry;
-//
-//        return sum;
-//    }
+}
