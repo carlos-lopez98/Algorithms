9102a2766f873241f730343a1b7026fc5910ab37
Carlos <cjimenez9811@gmail.com>
2023-10-01 09:42:02 -0700

    Remove nth Node

--- src/main/java/org/example/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/example/RemoveNthNodeFromEndOfList.java
@@ -5,6 +5,14 @@ package org.example;
 *
 * Given a singly linked list, remove the nth node from the end of the list and return its head
 *
+* Meaning remove the 3rd from last, etc...
+*
+* Not the nth position in the list
+*
 * */
 public class RemoveNthNodeFromEndOfList {
+
+
+
+
 }
