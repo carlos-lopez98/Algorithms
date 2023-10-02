a6a080d21bb523f5ad14b7339dd7fca6993daa61
Carlos <cjimenez9811@gmail.com>
2023-10-02 13:54:57 -0700

    ReverseWordsInAString Solution Explained

---
+++ src/main/java/org/example/ReverseWordsinAString.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+/*
+*
+* Given a sentence, reverse the order of its words without affecting the order of letters within a given word.
+*
+*
+* Sentence contains English uppercase and lowercase letters, digits, and spaces.
+* 1≤sentence.length ≤10^4
+* The order of the letters within a word is not to be reversed.
+*
+*
+* Note: The input string may contain leading or trailing spaces or multiple spaces between words. The returned string,
+* however, should only have a single space separating each word. Do not include any extra spaces.
+* */
+public class ReverseWordsinAString {
+
+    //What I'm going to do, is have two pointers, one will stay at the beginning of the word
+    //While the other traverses till the end, save those indexes, than create a substring
+    //Then add that substring to the end of my new string to return
+
+
+}
