42b3d8c7dbd415f946b53a324feb509d7d0782bf
Carlos <cjimenez9811@gmail.com>
2023-10-02 10:43:21 -0700

    Remove nth Node

---
+++ src/main/java/org/example/SortColors.java
@@ -0,0 +1,32 @@
+package org.example;
+
+/*
+*
+* Given an array, colors, which contains a combination of the following three elements:
+* 0(representing red)
+* 1(representing white)
+* 2(representing blue)
+* Sort the array in place so that the elements of the same color are adjacent,
+* with the colors in the order of red, white, and blue. The function should return the same array.
+* */
+public class SortColors {
+
+
+    //This is technically the brute force solution
+    public static int[] sortColors(int[] colors){
+
+        for(int i = 0; i < colors.length; i++){
+            for(int j = i+1; j < colors.length; j++){
+
+                if(colors[i] > colors[j]){
+                    int temp = colors[i];
+                    colors[i] = colors[j];
+                    colors[j] = temp;
+                    break;
+                }
+            }
+        }
+
+        return colors;
+    }
+}
