921466911ce2f4b0b24eb33e415fc2b46428096b
Carlos <cjimenez9811@gmail.com>
2023-10-02 15:13:33 -0700

    IsPalindromeII working

--- src/main/java/org/example/ValidePalindromeII.java
+++ src/main/java/org/example/ValidePalindromeII.java
@@ -38,7 +38,14 @@ public class ValidePalindromeII {
 
 
     public boolean isPalindrome(String s){
+        int start = 0;
+        int end = s.length() - 1;
 
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+        }
 
         return true;
     }
