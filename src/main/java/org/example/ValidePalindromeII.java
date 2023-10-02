c9ec293f69006a7359677027a8f16fbd15fe8271
Carlos <cjimenez9811@gmail.com>
2023-10-02 15:09:09 -0700

    IsPalindromeII working

---
+++ src/main/java/org/example/ValidePalindromeII.java
@@ -0,0 +1,36 @@
+package org.example;
+
+/*
+*
+* Write a function that takes a string as input and checks whether
+* it can be a valid palindrome by removing at most one character from it.
+*
+* */
+
+public class ValidePalindromeII {
+
+    //You're going to have two pointers
+    //One will start at the end, one at the beginning
+    //If you run into characters that don't match, then you're going to
+    //Check each substring, you're going to check a substring without the character at position one
+    //THen check the substring without the character at position two
+    //If either return true, then a solution is possible, if both return false, then a solution
+    //is not possible
+
+
+    public boolean checkOneDeletion(String s){
+
+
+
+
+
+        return true;
+    }
+
+
+    public boolean isPalindrome(String s){
+
+
+        return true;
+    }
+}
