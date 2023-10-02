1a3f28317ad3b8a66173de69f0c8acf40ad527d7
Carlos <cjimenez9811@gmail.com>
2023-10-02 08:15:50 -0700

    Remove nth Node

--- src/main/java/org/example/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/example/RemoveNthNodeFromEndOfList.java
@@ -12,7 +12,15 @@ package org.example;
 * */
 public class RemoveNthNodeFromEndOfList {
 
+    //The easy solution to this is to iterate through the list once, get the size
+    //Then start a loop and stop at the size- n - 1 position, the position before your target
+    //node in order to remove it's reference
 
+
+    //The more trickier solution is to use two pointers, one pointer will be a fast pointer
+    //We're going to maintain a distance of N between the two pointers, so making it to when
+    //The fast pointer makes it to the end (null) the slow pointer will be n distance away from the
+    //end effectively landing us in the correct position
 
 
 }
