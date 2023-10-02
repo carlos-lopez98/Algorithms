c169f839c8c8a870a422e591d836bf2bd520ec6f
Carlos <cjimenez9811@gmail.com>
2023-10-02 15:12:38 -0700

    IsPalindromeII working

--- src/main/java/org/example/ValidePalindromeII.java
+++ src/main/java/org/example/ValidePalindromeII.java
@@ -19,11 +19,20 @@ public class ValidePalindromeII {
 
 
     public boolean checkOneDeletion(String s){
+        int start = 0;
+        int end = s.length() - 1;
 
+        while(start <= end){
 
+            if(s.charAt(start) != s.charAt(end)){
 
+                return isPalindrome(s.substring(start + 1, end+1)) || isPalindrome(s.substring(start, end));
+            }
 
-
+            //Move Pointers Inward
+            start++;
+            end--;
+        }
         return true;
     }
 
