99b5b3963a172770f2a325e1b84fa54ec630cda9
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-10-06 08:19:37 -0700

    BubbleSort Working

--- src/main/java/org/example/arrays/BubbleSort.java
+++ src/main/java/org/example/arrays/BubbleSort.java
@@ -1,10 +1,36 @@
 package org.example.arrays;
 
+
+/*
+ *
+ * Create sorting methods, that sort descending left to right, descending right to left
+ * Create sorting method that sorts descending left to right
+ *
+ * */
+
 public class BubbleSort {
 
-    public void sort(int[] array){
+    int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+    public void sortAscendingLeftToRight(int[] array) {
+
+        for (int lastUnsorrtedIndex = intArray.length - 1; lastUnsorrtedIndex > 0; lastUnsorrtedIndex--) {
 
 
+        }
+    }
+
+
+    private void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        //Basic swapping algorithm
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 
     }
 }
