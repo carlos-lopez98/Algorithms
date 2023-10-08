51422c44bc52fe41ae2c6c77fa8ce82f9d15cd20
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-10-07 18:05:42 -0700

    bubble sort working

--- src/main/java/org/example/arrays/BubbleSort.java
+++ src/main/java/org/example/arrays/BubbleSort.java
@@ -24,16 +24,29 @@ public class BubbleSort {
     }
 
 
-    private static void swap(int[] array, int i, int j) {
+    public static void sortAscendingRighToLeft(int[] intArray){
 
+    //Here we basically want our unsorted index to go up to the start point, since we're right to left
+    for(int lastUnsortedIndex = intArray[0]; lastUnsortedIndex < intArray.length; lastUnsortedIndex++){
+
+        //Pointer moves from right to left, sorting in ascending order from left to right
+        for(int i = intArray.length - 1; i > 0; i--){
+         if(intArray[i] < intArray[i - 1]) {
+             swap(intArray, i, i - 1);
+         }
+        }
+    }
+
+
+    }
+
+    private static void swap(int[] array, int i, int j) {
         if (i == j) {
             return;
         }
-
         //Basic swapping algorithm
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-
     }
 }
