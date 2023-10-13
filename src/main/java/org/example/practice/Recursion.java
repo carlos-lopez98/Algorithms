b1e1de52cc042e311cea2c525a64657ac263ef06
Carlos <cjimenez9811@gmail.com>
2023-10-12 19:09:52 -0700

    Recursion Practice

--- src/main/java/org/example/practice/Recursion.java
+++ src/main/java/org/example/practice/Recursion.java
@@ -12,13 +12,11 @@ public class Recursion {
     //Implement Recursive Factorial
     //
 
-
-
-    public static int fact(int x){
-        if(x == 0){
+    public static int fact(int num){
+        if(num == 0){
             return 1;
         }
 
-        return x * fact(x-1);
+        return num * fact(num - 1);
     }
 }
