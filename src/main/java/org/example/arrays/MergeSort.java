1e7acb3fd31eda6ee020c04512e9277a7b2df895
Carlos <cjimenez9811@gmail.com>
2023-10-13 07:48:35 -0700

    MergeSort Implementation

--- src/main/java/org/example/arrays/MergeSort.java
+++ src/main/java/org/example/arrays/MergeSort.java
@@ -12,11 +12,24 @@ public class MergeSort {
             return;
         }
 
+
+
         //partition array
         int midpoint = (start + end)/2;
 
         mergeSort(input, start, midpoint);
         mergeSort(input, midpoint, end);
+        merge(input, start, midpoint, end);
+    }
+
+    public static void merge(int[] input, int start, int midpoint, int end){
+
+        //If the last element in the first partition is less than or equal to the first element in the right position
+        //That means all of the elements on the left are sorted, since this will happen first -- to individual elements
+        if(input[midpoint-1] <= input[midpoint]){
+            return;
+        }
+
 
     }
 }
