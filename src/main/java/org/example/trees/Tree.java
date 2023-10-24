d2b1fcb23272aaf7fcabe3a5694246afc94c961d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-10-24 09:28:25 -0700

    Trees Practice

--- src/main/java/org/example/trees/Tree.java
+++ src/main/java/org/example/trees/Tree.java
@@ -1,68 +1,29 @@
 package org.example.trees;
-
-
 /*
 *
 * This class is meant to be the start of our tree
 * We only hold a reference to the root node
 *
 * */
+
 public class Tree {
+
     public TreeNode root;
 
 
-    public Tree(){
-    }
-    public Tree(TreeNode root){
-        this.root = root;
-    }
+    //This will just be a method that prints out our Tree in order
+    //We must print left most children, then root, then right most child
+    public void inOrderTraversal(TreeNode root){
 
-    //This is post order Traversal, you'll end up traversing all the children nodes
-    //before the parent node is addressed
-    public static void postOrderTraversal(TreeNode root){
-
-        if(root != null){
-
-            if(root.children != null){
-                for(TreeNode node: root.children){
-                    postOrderTraversal(node);
-                }
-            }
-
+        if(root.children.isEmpty()){
             System.out.println(root.name);
-        }
-    }
+            return;
+        }else{
 
-    //Visits the nodes in ascending order/ left-right, so left branch, parent, then right branch
-    public static void inOrderTraversal(TreeNode root){
-        //Implementing this with nodes, that have a list reference to children is a bit tricky
 
-        if(root != null){
-            if(!root.children.isEmpty()){
-                //Go Left
-                inOrderTraversal(root.children.get(0));
-
-                //Retrieve current
-                System.out.println(root.name);
-
-                //Go Right
-                for(int i = 1; i < root.children.size(); i++){
-                    inOrderTraversal(root.children.get(i));
-                }
-            }else{
-                System.out.println(root.name);
-            }
-        }
-    }
-
-    //Visits the current nodes before the children
-    public static void preOderTraversal(TreeNode root){
-
-        if(root != null){
-            System.out.println(root.name);
-            for(TreeNode node: root.children){
-                preOderTraversal(node);
-            }
+         for(TreeNode current: root.children){
+             inOrderTraversal(current);
+         }
         }
     }
 }
