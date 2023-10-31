e044b0f453c58c8be26a0b0cb926f65d975bca96
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-10-31 07:30:59 -0700

    Binary Search Tree Working

--- src/main/java/org/example/trees/BinarySearchTree.java
+++ src/main/java/org/example/trees/BinarySearchTree.java
@@ -1,4 +1,28 @@
 package org.example.trees;
 
 public class BinarySearchTree {
+
+    private BinarySearchTreeNode root;
+
+    private BinarySearchTree(BinarySearchTreeNode root){
+        this.root = root;
+    }
+
+
+    //Now you need a method to insert into the tree once you've already initialized it;
+    //You can make the constructor private, then just call the constructor if needed
+    public void insert(BinarySearchTreeNode node){
+        if(this.root == null){
+            root = node;
+            return;
+        }
+
+        //We're going to need to go down tree, and find the spot to insert
+        this.root
+
+
+
+    }
+
+
 }
