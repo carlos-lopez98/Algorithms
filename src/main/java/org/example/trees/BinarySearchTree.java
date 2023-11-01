1c1c50dfdd6d1ff1d3e88fda89c710091e1d9cf1
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-01 05:52:57 -0700

    Binary Search Trees Working

--- src/main/java/org/example/trees/BinarySearchTree.java
+++ src/main/java/org/example/trees/BinarySearchTree.java
@@ -3,9 +3,21 @@ package org.example.trees;
 public class BinarySearchTree {
 
     //What variables should our BinarySearchTree contain?
-
+    BinarySearchTreeNode root;
     //Do we need a constructor in this case?
+    public BinarySearchTree(BinarySearchTreeNode root){
+        this.root = root;
+    }
 
     //What will our insert method do? Will the traversal be done here or in a lower level class?
+    public void insert(int value){
+        if(this.root == null){
+            root = new BinarySearchTreeNode(value);
+            return;
+        }
+
+        root.insert(value);
+    }
+
 
 }
--- src/main/java/org/example/trees/BinarySearchTreeNode.java
+++ src/main/java/org/example/trees/BinarySearchTreeNode.java
@@ -3,17 +3,38 @@ package org.example.trees;
 public class BinarySearchTreeNode {
 
     //What variables should our BinaryTreeNode store?
+    int data;
+    private BinarySearchTreeNode leftChild;
+    private BinarySearchTreeNode rightChild;
 
     //Should our BinaryTreeNode have a Constructor? Yes Or No?
-
+    public BinarySearchTreeNode(int value){
+        this.data = value;
+    }
 
     //What will our insert method do? Why isn't this implemented at a higher level?
-
+    public void insert(int value){
         //What will be our base case?
-
+        //We ignore duplicates
+        if(value == this.data){
+            return;
+        }
 
         //How do we approach searching left and searching right?
-
-            //At what point is our data inserted?
-
+        //At what point is our data inserted?
+        if(value < this.data){
+            if(leftChild == null){
+                leftChild = new BinarySearchTreeNode(value);
+                return;
+            }else{
+             leftChild.insert(value);
+            }
+        }else{
+            if(rightChild == null){
+                rightChild = new BinarySearchTreeNode(value);
+                return;
+            }
+            rightChild.insert(value);
+        }
+    }
 }
