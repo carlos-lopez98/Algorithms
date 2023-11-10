6cd08d95b0f71accdb1a9131b8ea6994a7e5eaf2
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-10 07:23:07 -0800

    Working on Palindrome Number

--- src/main/java/org/example/PalindromeNumber.java
+++ src/main/java/org/example/PalindromeNumber.java
@@ -1,5 +1,29 @@
 package org.example;
 
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+ */
+
+
+//For some problems you have to think mathematically to get to the optimal solution
+//You can solve this one by converting the number to a string, and then iterating through it
+//How would you solve it if you couldn't iterate through it? This is where the mathematical thinking comes in
 public class PalindromeNumber {
 
 
