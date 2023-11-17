54027880b3814814161135c5ea246c7f96f90799
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-17 06:48:17 -0800

    Max SubArray Working

--- src/main/java/org/example/MaximumSubArray.java
+++ src/main/java/org/example/MaximumSubArray.java
@@ -2,12 +2,16 @@ package org.example;
 
 
 /*
-
 Given an integer array nums, find the subarray with the largest sum, and return its sum.
-
-
  */
 
 
 public class MaximumSubArray {
+
+    //Return the sum of the largest subarray
+    //We don't need to keep track of the indices
+
+
+
+
 }
