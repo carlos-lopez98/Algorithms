846da31aa337a4e034cbd7cf195c999407f77198
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-17 07:06:28 -0800

    MaxSubArray Solved

--- src/main/java/org/example/MaximumSubArray.java
+++ src/main/java/org/example/MaximumSubArray.java
@@ -5,33 +5,24 @@ package org.example;
 Given an integer array nums, find the subarray with the largest sum, and return its sum.
  */
 
-
+//What's the bruteforce approach?
 public class MaximumSubArray {
 
-    //Return the sum of the largest subarray
-    //We don't need to keep track of the indices
-
-    //This will keep track of the maximum sum so far
 
 
-    //This will keep track of the current running sum
-    //When does this get reset is the question
-
-    public int maxSubArray(int[] nums){
-        int maxSum = nums[1];
-        int currentRunningSum = nums[1];
-
-
-        for(int i = 1; i < nums.length; i++){
-
-            //This is effectively being our decision maker
-            currentRunningSum = Math.max(currentRunningSum + nums[i], nums[i]);
-
-            //This is storing the new max, within maxSum
-            maxSum = Math.max(currentRunningSum, maxSum);
-
-        }
-
-        return maxSum;
-    }
+//    public int maxSubArray(int[] nums) {
+//
+//        //What are the variables needed for optimal approach?
+//
+//
+//        //How many for loops for iteration will you need?
+//
+//
+//        //How do we decide when to start a new subArray?
+//
+//
+//        //When do we update our global sum?
+//
+//
+//    }
 }
