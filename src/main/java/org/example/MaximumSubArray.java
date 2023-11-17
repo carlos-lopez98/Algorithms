0bb00881d5e50be243ebafa9fd1535ff79267b57
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-17 06:49:23 -0800

    MaxSubArray Working

--- src/main/java/org/example/MaximumSubArray.java
+++ src/main/java/org/example/MaximumSubArray.java
@@ -11,6 +11,13 @@ public class MaximumSubArray {
     //Return the sum of the largest subarray
     //We don't need to keep track of the indices
 
+    //This will keep track of the maximum sum so far
+    int maxSum;
+
+    //This will keep track of the current running sum
+    //When does this get reset is the question
+    int currentRunningSum;
+
 
 
 
