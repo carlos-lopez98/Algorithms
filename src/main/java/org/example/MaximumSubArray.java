0b8617076f50e1eedf421231a5cc4b777a11b0ed
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-17 07:00:04 -0800

    MaxSubArray Solved

--- src/main/java/org/example/MaximumSubArray.java
+++ src/main/java/org/example/MaximumSubArray.java
@@ -12,13 +12,26 @@ public class MaximumSubArray {
     //We don't need to keep track of the indices
 
     //This will keep track of the maximum sum so far
-    int maxSum;
+
 
     //This will keep track of the current running sum
     //When does this get reset is the question
-    int currentRunningSum;
+
+    public int maxSubArray(int[] nums){
+        int maxSum = nums[1];
+        int currentRunningSum = nums[1];
 
 
+        for(int i = 1; i < nums.length; i++){
 
+            //This is effectively being our decision maker
+            currentRunningSum = Math.max(currentRunningSum + nums[i], nums[i]);
 
+            //This is storing the new max, within maxSum
+            maxSum = Math.max(currentRunningSum, maxSum);
+
+        }
+
+        return maxSum;
+    }
 }
