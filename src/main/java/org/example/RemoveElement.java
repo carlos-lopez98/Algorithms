d9a0156c110349792a48bb1f901ed8bb1b811e95
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-22 07:57:23 -0800

    RemoveElement Working

--- src/main/java/org/example/RemoveElement.java
+++ src/main/java/org/example/RemoveElement.java
@@ -28,16 +28,5 @@ public class RemoveElement {
     public int removeElement(int[] nums, int val){
         //Knowing the above makes the problem way easier
 
-        int index = 0;
 
-        for(int i = 0; i < nums.length; i++){
-
-         if(nums[i] != val){
-             nums[index] = nums[i];
-             index++;
-         }
-        }
-
-        return index;
-    }
 }
