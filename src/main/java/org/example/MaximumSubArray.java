6d1f30dd27a94734dcfd7c5f230c7e46912680ca
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-11-22 07:50:45 -0800

    Working MaxSubArray

--- src/main/java/org/example/MaximumSubArray.java
+++ src/main/java/org/example/MaximumSubArray.java
@@ -9,29 +9,18 @@ Given an integer array nums, find the subarray with the largest sum, and return 
 public class MaximumSubArray {
 
 
-
-    public int maxSubArray(int[] nums) {
-
-        //What are the variables needed for optimal approach?
-        int currentRunningSum = 0;
-        int globalMaxSum = 0;
-
-        //How many for loops for iteration will you need?
-        for(int i = 0 ; i < nums.length; i++){
-
-            currentRunningSum = Math.max(nums[i], currentRunningSum + nums[i]);
-
-            if(currentRunningSum > globalMaxSum){
-                globalMaxSum = currentRunningSum;
-            }
-
-        }
-
-        //How do we decide when to start a new subArray?
-
-
-        //When do we update our global sum?
-
-        return globalMaxSum;
-    }
+//    //What happens if nums is just negative numbers
+//    public int maxSubArray(int[] nums) {
+//
+//        //What are the variables needed for optimal approach?
+//
+//        //How many for loops for iteration will you need?
+//
+//
+//        //How do we decide when to start a new subArray?
+//
+//
+//        //When do we update our global sum?
+//
+//    }
 }
