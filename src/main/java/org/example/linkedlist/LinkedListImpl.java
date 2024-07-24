f5ca8f2e0da314d44c2b9da1b9807ed38e555d7d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-24 15:01:48 -0700

    working on linkedlist problems

--- src/main/java/org/example/linkedlist/LinkedListImpl.java
+++ src/main/java/org/example/linkedlist/LinkedListImpl.java
@@ -9,6 +9,7 @@ package org.example.linkedlist;
 //1. Linked lists have a head and tail node, the list can access at O(1) runtime
 //2. LinkedLists must traverse from the head to get to a specific node/ data, unless it's a doubly linked list
 //3. To remove a data point, you must remove the reference to that particular node in order to remove it from the list
+
 public class LinkedListImpl<T> {
 
     private LinkedListNodeImpl<T> head;
@@ -28,14 +29,54 @@ public class LinkedListImpl<T> {
         return size;
     }
 
-    public void addNode(LinkedListNodeImpl<T> node){
-        if(head.getData() == null){
-            head = node;
+    //This add method is only adding the new nodes to the beginning of the
+    //linkedlist
+    public void addData(T data){
+
+        LinkedListNodeImpl temp = new LinkedListNodeImpl(data);
+
+        if(isEmpty()){
+            head = temp;
             tail = head;
         }else{
-            LinkedListNodeImpl<T> next = head.getNext();
-            head = node;
+            temp.setNext(head);
+            head = temp;
+            size++;
         }
     }
 
+    //We're deleting the node that contains this data
+    //Edge Cases: what if head is null?
+    //How would you remove just the head node?
+    public void remove(T data){
+        //If the linkedlist is empty just return
+        if(isEmpty()){
+            return;
+        }
+
+        if(head.getData() == data){
+            head = head.getNext();
+            size--;
+        }else{
+            LinkedListNodeImpl previousNode = head;
+            LinkedListNodeImpl currentNode = head.getNext();
+
+            while(currentNode.getData() != data){
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+            }
+            previousNode.setNext(currentNode.getNext());
+            size--;
+        }
+    }
+
+
+    public boolean isEmpty(){
+
+        if(size == 0){
+            return true;
+        }
+
+        return false;
+    }
 }
