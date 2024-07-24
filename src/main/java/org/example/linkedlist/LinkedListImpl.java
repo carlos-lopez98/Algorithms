3faf61f4c17cfdd408400865644a2e13c144ebb4
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-24 10:35:34 -0700

    Working on implementing linkedlist

---
+++ src/main/java/org/example/linkedlist/LinkedListImpl.java
@@ -0,0 +1,13 @@
+package org.example.linkedlist;
+
+/**
+ *This class is to go over how to implement a linkedlist structure
+ */
+
+//Ask yourself what is needed in a linkedlist?
+//How does a linkedlist work exactly?
+//1. Linked lists have a head and tail node, the list can access at O(1) runtime
+//2. LinkedLists must traverse from the head to get to a specific node/ data, unless it's a doubly linked list
+//3. To remove a data point, you must remove the reference to that particular node in order to remove it from the list
+public class LinkedListImpl {
+}
