82c6ea1e7ba12c116c4dfb7ea21e0faca9150861
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-25 11:06:41 -0700

    Working on String Permutation

--- src/main/java/org/example/strings/PalindromPermutation.java
+++ src/main/java/org/example/strings/PalindromPermutation.java
@@ -17,7 +17,34 @@ import java.util.Map;
 
 public class PalindromPermutation {
 
-    public boolean algoOne(String string){
+    public static boolean checkPalindromePermutation(String string){
 
+
+
+        return false;
+    }
+
+    //This will return a value of 0-26 for alphabetical characters
+    //It will return a value of -1, for nonalphabetical characters
+    //In our checkPermutation function we can ignore if the value returned is -1
+    private static int getCharVaue(char c){
+        //Create the integer range
+        //Numerical value 'a' - 'z'
+        //Numerical value 'A' - 'Z'
+
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int A = Character.getNumericValue('A');
+        int Z = Character.getNumericValue('Z');
+
+        int val = Character.getNumericValue(c);
+
+        if(val <= z && val >= a){
+            return val - a;
+        }else if(val <= Z && val >= A){
+            return val - A;
+        }
+
+        return -1;
     }
 }
