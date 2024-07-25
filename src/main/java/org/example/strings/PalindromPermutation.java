a51d87a56872431af1d664ebed35f23f8278cc28
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-25 09:11:10 -0700

    Working on String Problems

---
+++ src/main/java/org/example/strings/PalindromPermutation.java
@@ -0,0 +1,23 @@
+package org.example.strings;
+
+/*
+* Given a string write a function to check if it's a permutation of a palindrome.
+* A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
+* The palindrome does not need to be limited to dictionary words. You can ignore casing and non-letter characters.
+* */
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+//Clarifying Questions:
+//What makes a palindrome? Think of as many examples as you can
+//You can ignore casing and non-letter characters - meaning you can remove them
+
+public class PalindromPermutation {
+
+    public boolean algoOne(String string){
+
+    }
+}
