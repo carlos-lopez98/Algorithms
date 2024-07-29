eef16519652f360b7ba537ac268ff9a4a7a6b1d8
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-29 14:44:33 -0700

    Working on Helper Methods

--- src/main/java/org/example/helper/Pair.java
+++ src/main/java/org/example/helper/Pair.java
@@ -1,6 +1,8 @@
 package org.example.helper;
 
-public class Pair<K,B> {
+import java.util.Objects;
+
+public class Pair<K,B> extends Object {
 
     K first;
     B second;
@@ -14,5 +16,17 @@ public class Pair<K,B> {
 
 
     @Override
-    public boolean equals()
+    public boolean equals(Object o) {
+
+        if (this == o) return true; // Check if it's the same instance
+        if (!(o instanceof Pair<?, ?>)) return false; // Type check
+        Pair<?, ?> pair = (Pair<?, ?>) o; // Safe cast
+
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
