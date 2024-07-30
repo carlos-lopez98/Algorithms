f218c66eaa6837f4523effea34a9fcc0da9f9a25
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-30 09:12:22 -0700

    Working on rotating matrix

---
+++ src/main/java/org/example/arrays/RotateMatrix.java
@@ -0,0 +1,24 @@
+package org.example.arrays;
+
+/*
+* Given an image that represents a N*N matrix, where each pixel in the image is represented by an integer.
+* write a method to rotate the image by 90 degrees. Can you do this in place?
+* */
+public class RotateMatrix {
+
+
+
+
+    public static int[][] rotateMatrixNotInPlace(int[][] matrix){
+
+
+        return new int[][];
+    }
+
+
+    public static int[][] rotateMatrixInPlace(int[][] matrix){
+
+
+        return new int[][];
+    }
+}
