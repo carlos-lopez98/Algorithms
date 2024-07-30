9535d0386d61d8cc34647c160ca1984d4aa99b6a
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-29 20:23:06 -0700

    Practice Problems

---
+++ src/main/java/org/example/strings/CheckPermutation.java
@@ -0,0 +1,24 @@
+package org.example.strings;
+
+import java.util.*;
+
+/*
+* Given two strings write a method to decide if one is a permutation of the other
+*
+* This means one string must have all characters in the other string
+* */
+
+
+public class CheckPermutation {
+
+    //Questions:
+    //What is the character set of the strings being used? ASCII character set only 128 characters
+    //Is "Dog" a permutation of "god"? Does casing matter? Casing does matter so "Dog" is not a permutation of "god"
+    //Do spaces matter? is "dog  " a permutation of "god    "? yes, spaces matter, so no need to handle extra spaces/ non-alphabetical characters
+
+ public static boolean checkStringPermutation(String stringOne, String stringTwo) {
+
+
+     return false;
+ }
+}
