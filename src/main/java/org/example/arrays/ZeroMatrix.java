eaa4344f1d8a6d96f3a784cb27a3d58184d24e29
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-31 13:31:53 -0700

    2-D Array Practice

---
+++ src/main/java/org/example/arrays/ZeroMatrix.java
@@ -0,0 +1,15 @@
+package org.example.arrays;
+
+/**
+ * Write an algorithm such that if an element in an M x N matrix is 0
+ * its entire row and column are set to 0.
+ */
+public class ZeroMatrix {
+
+    //The one thing you have to consider for this one is if you iterate through the array
+    //Then run into a zero then immediately after set the row and column to zero
+    //You'll run into a zero, that you placed there, unintentionally zeroing out even more rows/ columns
+    public void zeroMatrix(int[][] matrix){
+
+    }
+}
