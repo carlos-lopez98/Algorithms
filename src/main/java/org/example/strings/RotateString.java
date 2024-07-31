e17aa68507e0f328a263a6ae6776d8c0fb2fddf3
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-31 15:02:44 -0700

    String Practice

---
+++ src/main/java/org/example/strings/RotateString.java
@@ -0,0 +1,16 @@
+package org.example.strings;
+
+/**
+ * Assume you have a method substring() which checks if one word
+ * is a substring of another. Given two strings, s1 and s2, write code to check if s2
+ * is a rotation of s1 using only one call to isSubstring()
+ *
+ * INPUT:
+ * "erbottlewat" is a rotation of "waterbottle" return true
+ * "bcdea" is a rotation of "abcde" return true
+ * "bcade" is not a rotation of "abcde" return false
+ */
+public class RotateString {
+
+
+}
