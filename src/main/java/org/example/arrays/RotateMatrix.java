3e25ec293cfbf86097a68c21006ca581450a008e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-31 21:51:31 -0700

    Practice Matrix Transpose

--- src/main/java/org/example/arrays/RotateMatrix.java
+++ src/main/java/org/example/arrays/RotateMatrix.java
@@ -1,9 +1,9 @@
 package org.example.arrays;
 
 /*
-* Given an image that represents a N*N matrix, where each pixel in the image is represented by an integer.
-* write a method to rotate the image by 90 degrees. Can you do this in place?
-* */
+ * Given an image that represents a N*N matrix, where each pixel in the image is represented by an integer.
+ * write a method to rotate the image by 90 degrees. Can you do this in place?
+ * */
 
 public class RotateMatrix {
 
@@ -23,16 +23,27 @@ public class RotateMatrix {
     //These are the correct columns, but they're not in the right order
     //To get the flip done you can start performing swaps
 
-    public static int[][] rotateMatrixNotInPlace(int[][] matrix){
+    public static int[][] rotateMatrixNotInPlace(int[][] matrix) {
 
 
         return new int[2][2];
     }
 
 
-    public static int[][] rotateMatrixInPlace(int[][] matrix){
+    public static int[][] rotateMatrixInPlace(int[][] matrix) {
 
 
         return new int[2][2];
+    }
+
+
+    public static void transposeMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
 }
