de020dc94e8a1c610d3db8d992520c1d5032629d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-31 21:39:13 -0700

    Practice Matrix Transpose

--- src/main/java/org/example/arrays/RotateMatrix.java
+++ src/main/java/org/example/arrays/RotateMatrix.java
@@ -4,8 +4,21 @@ package org.example.arrays;
 * Given an image that represents a N*N matrix, where each pixel in the image is represented by an integer.
 * write a method to rotate the image by 90 degrees. Can you do this in place?
 * */
+
 public class RotateMatrix {
 
+
+    //[0, 1, 2, 3]                 [12, 8, 4, 0]
+    //[4, 5, 6, 7]         ->>>    [13, 9, 5, 1]
+    //[8, 9, 10, 11]               [14, 10, 6, 2]
+    //[12, 13, 14, 15]             [15, 11, 7, 3]
+
+
+    //When you transpose you get
+    //[0]
+    //[ , 5,]
+    //[ , ,10]
+    //[ , , ,15]
 
 
 
