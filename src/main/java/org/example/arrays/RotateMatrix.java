a20548bb0f15c0aa156a9f06dd229325b58cb1fa
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-07-31 21:44:08 -0700

    Practice Matrix Transpose

--- src/main/java/org/example/arrays/RotateMatrix.java
+++ src/main/java/org/example/arrays/RotateMatrix.java
@@ -14,13 +14,14 @@ public class RotateMatrix {
     //[12, 13, 14, 15]             [15, 11, 7, 3]
 
 
-    //When you transpose you get
-    //[0]
-    //[ , 5,]
-    //[ , ,10]
-    //[ , , ,15]
+    //When you transpose you get the below
+    //[0,4,8,12]
+    //[1,5,9,13]
+    //[2,6,10,14]
+    //[3,7,11,15]
 
-
+    //These are the correct columns, but they're not in the right order
+    //To get the flip done you can start performing swaps
 
     public static int[][] rotateMatrixNotInPlace(int[][] matrix){
 
