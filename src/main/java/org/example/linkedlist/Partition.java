b42a680cc095abfcea5a631ecbce3f4da35809f1
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-07 14:37:37 -0700

    LinkedList Practice

---
+++ src/main/java/org/example/linkedlist/Partition.java
@@ -0,0 +1,18 @@
+package org.example.linkedlist;
+
+/**
+ * Write code to partition a linked list around a value x, such that all nodes less than x
+ * Come before all nodes greater than or equal to x. (Important) the partition element x can appear anywhere in the
+ * "right partition" it does not need to appear between the left and right partitions.
+ *
+ * Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
+ *
+ * OutPut: 3 -> 1 -> 2       ->      10 -> 5 -> 5 -> 8
+ *
+ * Notice how 5 does not have to be in the center, it's just left partition is elements less than 5
+ * Right partition is elements greater than 5
+ */
+public class Partition {
+
+
+}
