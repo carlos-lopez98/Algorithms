e98257ccfdf6e16988b68fc85602999efcdd1605
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-09 15:25:36 -0700

    ReverseLinkedList Practice

--- src/main/java/org/example/practice/ReverseLinkedList.java
+++ src/main/java/org/example/practice/ReverseLinkedList.java
@@ -3,33 +3,39 @@ package org.example.practice;
 public class ReverseLinkedList {
 
 
-    public static Node reverseList(Node node){
+    public static Node reverseList(Node node) {
+        //First create a pointer to our new reversed list
         Node head = null;
 
-        //While the node is not null
-        //Create a new node
-        //set that new node's value = to the current data
-        //then it's next value equal to our current head
-        //then  move the head pointer up to the new node
+        //Start iterating through our input list
+        while (node != null) {
 
-        //You effectively add elements in reverse
-        while(node != null){
+            //Create a new node to create the links
             Node n = new Node(node.data);
+            //Link our new node to our head, head value will then change
             n.next = head;
+            //Move head up, this effectively adds elements in reverse
+            //So if you have 1, 2, 3, 4
+
+            //You'll add 1 at the end
+            //Then add 2 -> 1
+            //3 -> 2 -> 1
+            //4 -> 3 -> 2 -> 1
             head = n;
+
+            //Move pointer up for iteration
             node = node.next;
         }
-
         return head;
     }
 }
 /**
- *To reverse a linkedlist, you have to think for a second
+ * To reverse a linkedlist, you have to think for a second
  * Usually when you go about it, you'll add a connection to the tail node
  * Then move tail up
  * But what would happen if you create a new node, set it's next = to your current head
  * Then move head up
- *
+ * <p>
  * You'll effectively, be adding nodes to the front of the list, instead of the back
  * So if you receive, a head node of a linked list, you can effectively reverse the list
  */
