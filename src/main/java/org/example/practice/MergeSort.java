b4dfddd3615fd23632b218deb5b63d950c7b6ee5
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-09 12:16:27 -0700

    MergeSort Practice

--- src/main/java/org/example/practice/MergeSort.java
+++ src/main/java/org/example/practice/MergeSort.java
@@ -3,6 +3,7 @@ package org.example.practice;
 public class MergeSort {
 
 
+    // {2, 5, 3, 4, 1,7,8,10}
     public static void mergeSortAlgo(int[] nums, int start, int end) {
 
         //Base case, as this is a recursive algorithm
@@ -12,12 +13,23 @@ public class MergeSort {
 
         int midpoint = (start + end) / 2;
 
+        //Partition all the way left
         mergeSortAlgo(nums, start, midpoint);
+
+        //Partition all the way right
         mergeSortAlgo(nums, midpoint, end);
         merge(nums, start, midpoint, end);
     }
 
     public static void merge(int[] nums, int start, int mid, int end) {
+
+        //Creates our temp array, used to store our temp values
+        //Size end - start, gives us the correctly sized array
+        int[] temp = new int[end - start];
+
+
+
+
 
 
     }
