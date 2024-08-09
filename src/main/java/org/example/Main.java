d76a04bd4588dbb41aca96dda6ee56e01897968b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-09 13:56:59 -0700

    MergeSort Practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,15 @@ import org.example.linkedlist.DeleteMiddleNode;
 import org.example.linkedlist.Partition;
 import org.example.linkedlist.RemoveDups;
 import org.example.linkedlist.ReturnKthToLast;
+import org.example.practice.MergeSort;
 import org.example.practice.Node;
 import org.example.practice.Recursion;
 import org.example.strings.*;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+
+import static org.example.practice.MergeSort.mergeSortAlgo;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,31 +27,34 @@ public class Main {
         };
 
 
-        Node head = new Node(6);
-        Node one = new Node(17);
-        Node two = new Node(10);
-        Node three = new Node(1);
-        Node four = new Node(29);
-
-        head.next = one;
-        one.next = two;
-        two.next = three;
-        three.next = four;
-
-
-        DeleteMiddleNode.deleteNode(two);
-
-        Node current = head;
-
-        while(current != null){
-            System.out.println(current.data);
-            current = current.next;
-        }
-
-//        int[] nums = {2, 5, 3, 4, 1,7,8,10};
+//        Node head = new Node(6);
+//        Node one = new Node(17);
+//        Node two = new Node(10);
+//        Node three = new Node(1);
+//        Node four = new Node(29);
 //
-//        ShellSort.shellSortDescending(nums);
+//        head.next = one;
+//        one.next = two;
+//        two.next = three;
+//        three.next = four;
 //
-//        System.out.println(Arrays.toString(nums));
+//
+//        DeleteMiddleNode.deleteNode(two);
+//
+//        Node current = head;
+//
+//        while(current != null){
+//            System.out.println(current.data);
+//            current = current.next;
+//        }
+
+
+        int[] nums = {2, 5, 3, 4, 1,7,8,10};
+
+        mergeSortAlgo(nums, 0, nums.length);
+//        org.example.arrays.MergeSort.mergeSort(nums, 0, nums.length);
+
+
+        System.out.println(Arrays.toString(nums));
     }
 }
--- src/main/java/org/example/practice/MergeSort.java
+++ src/main/java/org/example/practice/MergeSort.java
@@ -7,12 +7,11 @@ public class MergeSort {
     public static void mergeSortAlgo(int[] nums, int start, int end) {
 
         //Base case, as this is a recursive algorithm
-        if (start - end < 2) {
+        if (end - start < 2) {
             return;
         }
 
         int midpoint = (start + end) / 2;
-
         //Partition all the way left
         mergeSortAlgo(nums, start, midpoint);
 
@@ -23,15 +22,54 @@ public class MergeSort {
 
     public static void merge(int[] nums, int start, int mid, int end) {
 
+        //Since each iteration is getting sorted as they merge
+        //If an element on the right is greater than an element on the left
+        //That automatically means, any elements further right will also be greater
+        //Since we're sorting as we merge
+
+        //If they're equal then obviously they're also in the correct position
+        if(nums[mid] >= nums[mid -1]){
+            return;
+        }
+
         //Creates our temp array, used to store our temp values
         //Size end - start, gives us the correctly sized array
-        int[] temp = new int[end - start];
+        int[] tempArray = new int[end - start];
 
+        //These will be our pointers, i
+        int i = start;
+        int j = mid;
+        int temp = 0;
 
+        while(i < mid && j < end){
 
+            //Having the equals, handles cases where elements are equal
+            //If we don't put the equal then we'll miss elements
+            //Something won't get added into our temp array
+            if(nums[i] <= nums[j]){
+                tempArray[temp] = nums[i];
+                i++;
+            }else if(nums[i] > nums[j]){
+                tempArray[temp] = nums[j];
+                j++;
+            }
+            temp++;
+        }
 
+        //Here we need to copy the any elements remaining on the left partition into the right partition
+        //That means our copy position will be at temp, since we've been increasing temp for every element
+        //That is inserted
+        // {1, 5} {3, 4}
 
+        // {1, 3, 4}
 
+        //The below will copy over the five
+
+        //This will copy any remaining elements in the left partition
+        System.arraycopy(nums, i, nums, start + temp, mid - i);
+
+        //This will copy everything over from temp to nums
+        System.arraycopy(tempArray, 0, nums, start, temp);
     }
 }
 
