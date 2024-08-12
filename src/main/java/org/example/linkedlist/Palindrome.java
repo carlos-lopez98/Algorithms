9252999d0f01687cb82f5881cac4feffc54d21ad
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-12 15:34:48 -0700

    LinkedList Practice

---
+++ src/main/java/org/example/linkedlist/Palindrome.java
@@ -0,0 +1,21 @@
+package org.example.linkedlist;
+
+import org.example.practice.Node;
+
+public class Palindrome {
+
+    public static boolean isLinkedListPalindrome(Node head){
+
+
+       Node reverse = reverseLinkedList(Node head);
+
+        return false;
+    }
+
+    //Here's where we have to imagine putting in elements at the head
+    //Effectively reversing our list
+    public static Node reverseLinkedList(Node head){
+
+
+    }
+}
