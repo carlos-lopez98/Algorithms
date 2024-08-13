cd24abd88c9c20550b92badfb297a581f3add393
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-13 08:04:27 -0700

    MergeSort Practice

--- src/main/java/org/example/practice/MergeSort.java
+++ src/main/java/org/example/practice/MergeSort.java
@@ -1,6 +1,19 @@
 package org.example.practice;
 
 public class MergeSort {
+    //Partition the Array - This will be a recursive call
+    //On the way back from the call stack, you'll merge
+
+    //Merge function steps
+    //1. Only handle merges when the right side is less than the left side, depending on how you're sorting
+    //2. Create a temp array - This will store your elements in the correct order for the time being
+    //3. You want two pointers - one on the left and one on the right
+    // - If the one on the right is less than the one on the left, the one on the right
+    // will be placed in the correct position on the temp array - for this you'll need a temp index
+    // after the pointer on the right moves forward
+
+    //Lastly, you'll merge your temp array back into the original array, take a moment to think how this will happen
+    //You can
 
 
     // {2, 5, 3, 4, 1,7,8,10}
@@ -65,7 +78,7 @@ public class MergeSort {
 
         //The below will copy over the five
 
-        //This will copy any remaining elements in the left partition
+        //This will copy any remaining elements in the left partition - that weren't copied over to the temp array
         System.arraycopy(nums, i, nums, start + temp, mid - i);
 
         //This will copy everything over from temp to nums
