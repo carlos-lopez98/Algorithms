3d0c17e9374c4416cbf086b7d3c793c341ae1ab0
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-15 10:27:16 -0700

    Stack Practice

--- src/main/java/org/example/stacks/ThreeInOne.java
+++ src/main/java/org/example/stacks/ThreeInOne.java
@@ -1,4 +1,24 @@
 package org.example.stacks;
 
+
+/**
+ * Implement three stacks using an array
+ */
 public class ThreeInOne {
+
+
+
+
+
+    private class StackOne{
+
+    }
+
+    private class StackTwo{
+
+    }
+
+    private class StackThree{
+
+    }
 }
