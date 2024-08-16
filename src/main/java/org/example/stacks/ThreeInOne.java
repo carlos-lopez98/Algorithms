7fb465707238f59fa9c3849936fa0d694799e042
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-16 08:29:14 -0700

    Stack Practice

--- src/main/java/org/example/stacks/ThreeInOne.java
+++ src/main/java/org/example/stacks/ThreeInOne.java
@@ -10,14 +10,24 @@ public class ThreeInOne {
     public int[] threeStack;
 
 
+    //This will initialize our max stack size per stack
+    public ThreeInOne(int stackSize){
+        threeStack = new int[3 * stackSize];
+
+    }
+
+
+    //Offset for this one is 0
     private class StackOne{
 
     }
 
+    //Offset for this one is 1
     private class StackTwo{
 
     }
 
+    //Offset for this one is 2
     private class StackThree{
 
     }
