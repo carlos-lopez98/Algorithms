803d4b02ca3f17df820b3d14e6b9141667177c3b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-16 10:27:04 -0700

    Stack Practice

--- src/main/java/org/example/StackMin.java
+++ src/main/java/org/example/StackMin.java
@@ -8,12 +8,15 @@ package org.example;
 * */
 
 public class StackMin {
-    //In order to do this you need a way to find the minimum
-    //You can store the minimum value as an int, in your stack
 
-    //Second Method, is two implement one stack for tracking all your values
-    //Another stack keeps track of only minimum, when you add a new value
-    //You do a stack_min.peek() to get your most recent minimum, and if this new value
-    //is lower then or equal to you push to all values and min stack -- you want to do the equal check
-    //to account for multiple duplicates, IE if you have 2,2,2,2 your min stack should also look like 2,2,2,2
 }
+
+/**
+ * This one would be easy you just create an extra variabe besides top
+ *
+ * You could call this one min, everytime you push to the stack you just verify if this new value is the new min
+ * Then if it is set it equal to min.
+ *
+ * Then when calling min, you just return the min value, this won't be able to pop() thoush, as the min value can be
+ * In a position not at the top of our stack
+ */
