06ff08880b1a9bffebb3dfea8f6eec46953d7f87
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-19 08:40:54 -0700

    Added OneAway to Test One

--- src/main/java/Testing/TestSetOne/OneAway.java
+++ src/main/java/Testing/TestSetOne/OneAway.java
@@ -1,4 +1,8 @@
 package Testing.TestSetOne;
 
+/**
+ * There are three types of edits that can be performed on a string. Insert a character, remove a character, or
+ * replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+ */
 public class OneAway {
 }
