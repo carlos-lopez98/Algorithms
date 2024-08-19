a62b14096cf5d05199e054a4395303bb13eb10d7
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-19 08:45:05 -0700

    Added Partition  to Test One

--- src/main/java/Testing/TestSetOne/Partition.java
+++ src/main/java/Testing/TestSetOne/Partition.java
@@ -1,4 +1,14 @@
 package Testing.TestSetOne;
 
+/**
+ * Write code to partition a linkedlist around a value x, such that all nodes less than x come before all nodes
+ * greater than or equal to x. (IMPORTANT) The partition element x can appear anywhere in the "right partition"
+ * , it does not need to appear between the left and right partitions. The additional spacing in the example below
+ * indicates the partition. Yes, the output below is one of many valid outputs!
+ *
+ * 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 (partition = 5)
+ *
+ * 3 ->1 -> 2 -> 10 -> 5 -> 5 -> 8
+ */
 public class Partition {
 }
