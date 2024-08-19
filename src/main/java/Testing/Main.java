43de0a941b31cee7a692f01f1a0589a4b034df68
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-19 09:49:16 -0700

    Promblem IsPalindrome Complete

--- src/main/java/Testing/Main.java
+++ src/main/java/Testing/Main.java
@@ -1,30 +1,46 @@
 package Testing;
 
 import Testing.TestSetOne.Intersection;
+import Testing.TestSetOne.IsPalindrome;
 import org.example.practice.Node;
 
 public class Main {
     public static void main(String[] args) {
 
-        Node one = new Node(5);
-        Node two = new Node(7);
-        Node three = new Node(10);
-        Node four = new Node(2);
-        Node five = new Node(17);
+        /**
+         * Test Set Problem One Intersection
+         */
+//        Node one = new Node(5);
+//        Node two = new Node(7);
+//        Node three = new Node(10);
+//        Node four = new Node(2);
+//        Node five = new Node(17);
+//
+//        Node second = new Node(19);
+//        Node third = new Node(21);
+//
+//
+//        one.next = two;
+//        two.next = three;
+//        three.next = four;
+//        four.next = five;
+//
+//        second.next = third;
+//        third.next = four;
+//
+//        System.out.println(Intersection.returnIntersecting(one, second).data);
 
-        Node second = new Node(19);
-        Node third = new Node(21);
+                Node one = new Node(1);
+                Node two = new Node(1);
+                Node three = new Node(15);
+                Node four = new Node(1);
+                Node five = new Node(3);
 
+            one.next = two;
+            two.next = three;
+            three.next = four;
+            four.next = five;
 
-        one.next = two;
-        two.next = three;
-        three.next = four;
-        four.next = five;
-
-        second.next = third;
-        third.next = four;
-
-        System.out.println(Intersection.returnIntersecting(one, second).data);
-
+        System.out.println(IsPalindrome.isPalindrome(one));
     }
 }
--- src/main/java/Testing/TestSetOne/IsPalindrome.java
+++ src/main/java/Testing/TestSetOne/IsPalindrome.java
@@ -1,7 +1,44 @@
 package Testing.TestSetOne;
 
+import org.example.practice.Node;
+
 /**
  * Implement a function to check if a linkedlist is a palindrome.
  */
 public class IsPalindrome {
+    static Node reversed;
+
+    public static boolean isPalindrome(Node head){
+        if(head == null){
+            return false;
+        }
+
+        reverseList(head);
+        while(reversed != null){
+            if(reversed.data != head.data){
+                return false;
+            }
+            reversed = reversed.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+
+    //This will reverse the list, as you're adding to the head and not the tail
+    public static void reverseList(Node head){
+
+        while(head != null){
+            Node current = new Node(head.data);
+            current.next = reversed;
+            reversed = current;
+            head = head.next;
+        }
+
+    }
 }
+
+/**
+ * One way to check if something is a palindrome is to just reverse it, then compmare the new item with the old item
+ */
