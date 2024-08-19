6c81b20cd6e7eb9e64eb7649114d9193dfaae65b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-19 08:46:09 -0700

    Added Rotate90Degrees to Test One

--- src/main/java/Testing/TestSetOne/Rotate90Degrees.java
+++ src/main/java/Testing/TestSetOne/Rotate90Degrees.java
@@ -1,4 +1,8 @@
 package Testing.TestSetOne;
 
+/**
+ * Given an image represented by NXN matrix, where each pixel in the image is represented by an integer, write a method
+ * to rotate the image by 90 degrees. Can you do this in place?
+ */
 public class Rotate90Degrees {
 }
