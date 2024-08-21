3069d568f98a6b044e7f8f18f812ac4a97c54eee
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-20 18:25:06 -0700

    Problem RotateString Working

--- src/main/java/Testing/TestSetOne/RotateString.java
+++ src/main/java/Testing/TestSetOne/RotateString.java
@@ -6,4 +6,34 @@ package Testing.TestSetOne;
  * rotation of "erbottlewat"
  */
 public class RotateString {
+
+
+    public static boolean isRotation(String one, String two){
+        //Lengths must be equal in order for the second string to be a rotation of the first string
+        if(one.length() != two.length()){
+            return false;
+        }
+
+
+        for(int i = 0; i < one.length(); i++){
+
+        }
+    }
+
+    public static boolean checkRotationAtPoint(String one, String two, int rotation){
+
+
+
+
+    }
 }
+
+/**
+ * This one is best done using the trick where you can start an iteration at different points in a string
+ *
+ * Trick is done by giving a function different starting positions and using % the string's length as the start point
+ * Envision an outer loop that passes is values 0 - string length as the starting point, once starting point is taken
+ * the second for loop will always start the first string comparison at position 0 while the second comparison starts
+ * at start point % string length, when the second pointer reaches the end of the string, it's going to be reset to,
+ * input start point % string length
+ */
