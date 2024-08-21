917a01194c38739aa37766958f39d90c1dd2e433
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-20 21:21:42 -0700

    Problem Urlify Working

--- src/main/java/Testing/TestSetOne/Urlify.java
+++ src/main/java/Testing/TestSetOne/Urlify.java
@@ -8,9 +8,7 @@ package Testing.TestSetOne;
 public class Urlify {
 
 
-
-    public static char[] url(char[] s){
-
+    public static char[] url(char[] s) {
 
 
     }
@@ -22,10 +20,16 @@ public class Urlify {
  * %20, the reason we're being given extra white spaces is because they want us to perform swaps, the problem gives it
  * away, but the caveat, is that they're saying they're giving enough white space. That means that the string
  * may have extra white spaces we don't need.
- *
+ * <p>
  * With this in mind we can start, in order to do this in place, we want to swap values from the back of the string
  * to a pointer starting in the white spaces. To figure out the start positions we have to think for a second.
- *
+ * <p>
  * We're being give our true length, so that will be the start position for the string pointer, the white space pointer
  * will be determined by the amount of whitespaces we need in order to perform the swaps.
+ * If our phrase has 8 white spaces in between, how many white spaces at the end will we need, we need two extra spaces
+ * for every space, so we'll need 16 extra spaces. So our start pointer position in the white space will be true length
+ * plus the white space * 2
+ * <p>
+ * Then we'll just perform swaps at every non-white space, and if we reach a white space we insert, %20, then move our
+ * pointer down by three instead of one.
  */
