1169d19fef029071cb7fe4a99e071df678f1fcdd
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-22 08:14:28 -0700

    MyQueue Problem Complete

--- src/main/java/org/example/stacks/MyQueue.java
+++ src/main/java/org/example/stacks/MyQueue.java
@@ -35,5 +35,9 @@ public class MyQueue {
     }
 }
 /**
- * With this one you have to think
+ * With this one you have to think about the order that queues and stack add and remove elements. If you think about it
+ * When you add to a stack you remove the most recently added element. For a queue, you'd remove the first element added
+ * so in order to get this to work as a queue, you must reverse the order of the first stack. To do this, you'd
+ * just add the elements into a second stack, then pop the top element off the stack, then return the elements back to
+ * the first stack, and keep adding from the tail.
  */
