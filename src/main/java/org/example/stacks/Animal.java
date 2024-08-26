7f37b4efda0fea86ef5b75f70f22e22ce690c6e7
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-26 08:30:43 -0700

    Working on Animal Shelter Problem

--- src/main/java/org/example/stacks/Animal.java
+++ src/main/java/org/example/stacks/Animal.java
@@ -1,7 +1,26 @@
 package org.example.stacks;
 
+import java.time.LocalDateTime;
+
 /**
  * Class is needed for Animal Shelter Problem
  */
 public class Animal {
+
+    LocalDateTime timeArrived;
+
+    public class Dog extends Animal{
+
+        Dog(LocalDateTime timeArrived){
+            this.timeArrived = timeArrived;
+        }
+    }
+
+    public class Cat extends Animal{
+
+        Cat(LocalDateTime timeArrived){
+            this.timeArrived = timeArrived;
+        }
+    }
+
 }
