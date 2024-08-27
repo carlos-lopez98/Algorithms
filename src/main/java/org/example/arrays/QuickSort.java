81d896699c81f3032b9391b4a01fa90224af939c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-27 09:10:12 -0700

    QuickSort Working

--- src/main/java/org/example/arrays/QuickSort.java
+++ src/main/java/org/example/arrays/QuickSort.java
@@ -5,3 +5,10 @@ public class QuickSort {
 
 
 }
+/**
+ * We will basically divide and conquer our array/ structure
+ * based on a pivot element
+ *
+ * Anything less than the pivot will go left anything larger will go right
+ * You do this recursively enough times and you get a sorted array
+ */
