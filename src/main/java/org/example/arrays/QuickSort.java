8a13ac0165e396e81987a2bb58aaf9938239feba
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-27 09:14:37 -0700

    QuickSort Working

--- src/main/java/org/example/arrays/QuickSort.java
+++ src/main/java/org/example/arrays/QuickSort.java
@@ -4,6 +4,28 @@ public class QuickSort {
 
 
 
+    public static void quickSort(int[] input, int start, int end){
+        //This breaks at of our recursion when are array is one element in length
+        //No need to sort/ partition a single element array
+        if( end - start < 2){
+            return;
+        }
+
+
+        //After partitioning the array, this will return the position where our pivot element was placed
+        //This position is effectively sorted already by our partition method
+        int pivotIndex = partition(input, start, end);
+
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end){
+
+
+
+
+    }
 }
 /**
  * We will basically divide and conquer our array/ structure
