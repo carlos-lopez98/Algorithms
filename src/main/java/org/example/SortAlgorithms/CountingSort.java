c5c7487803667a65d92730abacfc4df6c1af41f9
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-28 09:20:22 -0700

    CountingSort Working

--- src/main/java/org/example/SortAlgorithms/CountingSort.java
+++ src/main/java/org/example/SortAlgorithms/CountingSort.java
@@ -12,4 +12,8 @@ public class CountingSort {
  * With the range included, we can create a temp array that stores the counts of numbers min to max
  *
  * With the counts, we are able to place the elements from min to max back into the input array in the correct order
+ *
+ * Runtime for this algorithm is O(n), Unstable not in-place, as we need to create a new array, but in-place technically
+ * since we're not creating new variables, just keeping track of their counts, so space is O(n) n being the range,
+ * So it can be argued that the space is O(1)
  */
