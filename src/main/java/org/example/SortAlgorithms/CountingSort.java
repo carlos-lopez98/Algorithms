5b3cf4f59bd9d4ca5aaa73d6592503f73013f5a2
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-08-28 09:19:03 -0700

    CountingSort Working

---
+++ src/main/java/org/example/SortAlgorithms/CountingSort.java
@@ -0,0 +1,15 @@
+package org.example.SortAlgorithms;
+
+public class CountingSort {
+
+
+}
+/**
+ * This Algorithm assumes the elements to be sorted are within a given range.
+ *
+ * The range will be passed into our method, as a max and min, so the range is max - min
+ *
+ * With the range included, we can create a temp array that stores the counts of numbers min to max
+ *
+ * With the counts, we are able to place the elements from min to max back into the input array in the correct order
+ */
