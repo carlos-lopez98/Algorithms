3577bd90f3250f90ce2396e841b8a884127ec19d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-09-10 09:01:12 -0700

    Working

--- src/main/java/org/example/SortAlgorithms/RadixSort.java
+++ src/main/java/org/example/SortAlgorithms/RadixSort.java
@@ -10,8 +10,8 @@ public class RadixSort {
     //This sorting algorithm assumes the radix and width for all values is the same
     public static void radSort(int[] input, int radix, int width){
 
+        //Will loop through the positions in our number
         for(int i = 0; i < width; i++){
-
             //Calling radixSingleSort for each position in the value
             radixSingleSort(input, i, radix);
         }
@@ -24,19 +24,38 @@ public class RadixSort {
 
         int[] countArray = new int[radix];
 
+        //Increment count for each value in the 1's
+        //Second iteration, increment count for the 10's
         for (int value: input){
             //Will return a digit at a position in our number
             countArray[getDigit(position, value, radix)]++;
         }
 
+        //Contains the number of values that have the specified digit or less than the specified digit
+        //Adjusting counts
+        for(int j = 1; j < radix; j++){
+            countArray[j] += countArray[j-1];
+        }
+
+        int[] temp = new int[numItems];
+
+        //Fill in temp array - with the sorted positions
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--){
+            temp[--countArray[getDigit(position,input[tempIndex], radix)]] =
+                    input[tempIndex];
+        }
+
+        //Copy temp array back into original array
+        for(int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            input[tempIndex] = temp[tempIndex];
+        }
+
     }
 
     public static int getDigit(int position, int value, int radix){
-
-
         //Will return the digit you want based on the position you're at,
         //IE if you want the digit in the hundred position, then you'd be at position 2, Math.pow would return 100,
         //The value would be let's say 4725/ 100 gives you 472
-        return value/ (int) Math.pow(10, position) % radix;
+        return value/ (int) Math.pow(radix, position) % radix;
     }
 }
