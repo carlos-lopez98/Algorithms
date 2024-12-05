0588b1d007b971d1d733790a6033d5bc4df81301
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2024-12-05 13:27:49 -0800

    Practice

--- src/main/java/org/example/SortAlgorithms/SelectionSort.java
+++ src/main/java/org/example/SortAlgorithms/SelectionSort.java
@@ -4,22 +4,8 @@ public class SelectionSort {
 
 
     public static void selectionSortAscendingLeftToRight(int[] array){
-        //Outer keeps track of our sorted places
-        //Or last unsorted index
-    for(int lastUnsortedIndex = array.length-1; lastUnsortedIndex > 0; lastUnsortedIndex--){
 
-        //This keeps track of our largest index
-        int max = 0;
 
-        //This is an inner iteration, looking for our max index
-        for(int i = 0; i <= lastUnsortedIndex; i++){
-            if(i == lastUnsortedIndex){
-                swap(array, max, i);
-            }else if(array[i] > array[max]){
-                max = i;
-            }
-        }
-    }
     }
 
 
