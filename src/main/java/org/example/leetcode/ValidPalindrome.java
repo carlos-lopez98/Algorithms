4881dcbc7c4c6f7481c4c4af19cb9cfbe9709705
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-02-20 11:21:21 -0800

    Valid Palindrome

--- src/main/java/org/example/leetcode/ValidPalindrome.java
+++ src/main/java/org/example/leetcode/ValidPalindrome.java
@@ -4,6 +4,33 @@ public class ValidPalindrome {
 
     public static boolean checkIfPalindrome (String s){
 
-        return false;
+        int start = 0;
+        int end = s.length() - 1;
+
+        String compareTo = s.toLowerCase();
+
+        //Creates two pointers one starting at the end, and one at the beginning
+        while(start < end){
+
+            //These two while loops ignore the non digit or letter characters
+            while(start < end && !Character.isLetterOrDigit(compareTo.charAt(start))){
+                start++;
+            }
+
+            while(start < end && !Character.isLetterOrDigit(compareTo.charAt(end))){
+                end--;
+            }
+
+
+            //If the position at start does not equal the position at end - then it's not a palindrome
+            if(compareTo.charAt(start) != compareTo.charAt(end)){
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
