3c37ff939d9fb4ca3a6c11dbfc3e0f94276de1d4
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-05 20:38:39 -0800

    Working on Problems

---
+++ src/main/java/org/example/leetcode/ContainerWithMostRainWater.java
@@ -0,0 +1,47 @@
+package org.example.leetcode;
+
+/**
+ * You are given an integer array height of length n.
+ * There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ * Constraints:
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ */
+
+public class ContainerWithMostRainWater {
+
+    //Input array is an array of multiple heights
+    public static int mostRainWater(int[] height){
+        int leftWall = 0;
+        int rightWall = height.length -1;
+
+        int maxArea = 0;
+
+        while(leftWall < rightWall){
+            int h = Math.min(height[leftWall], height[rightWall]);
+            int w = Math.abs(rightWall - leftWall);
+            int currArea = h * w;
+
+            if(height[leftWall] > height[rightWall]){
+                rightWall--;
+            }else if (height[leftWall] < height[rightWall]){
+                leftWall++;
+            }else{
+                leftWall++;
+                rightWall--;
+            }
+
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/main/java/org/example/leetcode/TrappingRainWater.java
@@ -0,0 +1,54 @@
+package org.example.leetcode;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it can trap after raining.
+ *
+ *Example 2:
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
+ *
+ * Constraints:
+ * n == height.length
+ * 1 <= n <= 2 * 104
+ * 0 <= height[i] <= 105
+ */
+public class TrappingRainWater {
+
+    public static int rainCapacity(int[] height){
+        //Uses the two pointer technique
+
+        int leftWall = height[0];
+        int rightWall = height.length - 1;
+
+        int water = 0;
+        int leftMax = -1;
+        int rightMax = -1;
+
+
+        while (leftWall < rightWall){
+
+            if(height[leftWall] <= height[rightWall]){
+
+                
+                if(height[leftWall] > leftMax){
+                    leftMax = height[leftWall];
+                }else{
+                    water += leftMax - height[leftWall];
+                }
+
+                leftWall++;
+            }else{
+                if(height[rightWall] > rightMax){
+                    rightMax = height[rightWall];
+                }else{
+                    water += rightMax - height[rightWall];
+                }
+
+                rightWall--;
+            }
+        }
+
+        return water;
+    }
+}
