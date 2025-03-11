eaaadaca1478c7bb883f14d2f513c114071bd598
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-11 16:47:01 -0700

    K Closest Elements

---
+++ src/main/java/org/example/leetcode/FindKClosestElements.java
@@ -0,0 +1,71 @@
+package org.example.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array.
+ * The result should also be sorted in ascending order.
+ *
+ * An integer a is closer to x than an integer b if:
+ *
+ * |a - x| < |b - x|, or
+ * |a - x| == |b - x| and a < b
+ *
+ * Example 1:
+ * Input: arr = [1,2,3,4,5], k = 4, x = 3
+ * Output: [1,2,3,4]
+ *
+ * Example 2:
+ * Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+ * Output: [1,1,2,3]
+ *
+ * Constraints:
+ * 1 <= k <= arr.length
+ * 1 <= arr.length <= 104
+ * arr is sorted in ascending order.
+ * -104 <= arr[i], x <= 104
+ */
+public class FindKClosestElements {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x){
+        List<Integer> ans = new ArrayList<>();
+
+        //Return all elements if k = arr.length
+        if(k == arr.length){
+            for(int i : arr){
+                ans.add(i);
+            }
+
+            return ans;
+        }
+
+
+        int left = 0;
+        int right = arr.length - k;
+
+        while(left < right){
+            int midpoint = left + (right - left)/2;
+
+            //This means that x is
+            if(x - arr[midpoint] > arr[midpoint + k] - x ){
+                left = midpoint + 1;
+            }else{
+                right = midpoint;
+            }
+
+
+
+
+
+        }
+
+        for(int i = left; i < left + k; i++){
+            ans.add(arr[i]);
+        }
+
+
+        return ans;
+    }
+}
