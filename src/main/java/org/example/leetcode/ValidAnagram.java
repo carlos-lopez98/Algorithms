6331fe92a00b4aa4004941b1cb147781ac0e9ea6
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-17 15:02:17 -0700

    Valid Anagram - Review

--- src/main/java/org/example/leetcode/ValidAnagram.java
+++ src/main/java/org/example/leetcode/ValidAnagram.java
@@ -10,47 +10,47 @@ import java.util.Set;
 public class ValidAnagram {
 
 
-    public static boolean isAnagram(String s, String t){
-        if(s.length() != t.length()){
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
             return false;
         }
 
         int[] array = new int[26];
 
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char current = s.charAt(i);
 
             array[getNumericalValue(current)] = Integer.sum(array[getNumericalValue(current)], 1);
         }
 
-        for(int j = 0; j < t.length(); j++){
+        for (int j = 0; j < t.length(); j++) {
             char current = t.charAt(j);
 
             array[getNumericalValue(current)] = Integer.sum(array[getNumericalValue(current)], -1);
 
-            if(array[getNumericalValue(current)] < 0){
+            if (array[getNumericalValue(current)] < 0) {
                 return false;
             }
         }
 
-     return true;
+        return true;
     }
 
-        public static int getNumericalValue(char c){
+    public static int getNumericalValue(char c) {
 
-            int A = Character.getNumericValue('A');
-            int Z = Character.getNumericValue('Z');
-            int a = Character.getNumericValue('a');
-            int z = Character.getNumericValue('z');
+        int A = Character.getNumericValue('A');
+        int Z = Character.getNumericValue('Z');
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
 
-            int curr = Character.getNumericValue(c);
+        int curr = Character.getNumericValue(c);
 
-            if(curr <= Z && curr >= A){
-                return curr - A;
-            } else if(curr <= z && curr >= a){
-                return curr - a;
-            }
-
-            return -1;
+        if (curr <= Z && curr >= A) {
+            return curr - A;
+        } else if (curr <= z && curr >= a) {
+            return curr - a;
         }
+
+        return -1;
+    }
 }
