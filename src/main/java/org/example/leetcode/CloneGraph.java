52bf3a27635804a05caec41678b6e323a0e91e50
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-03 13:13:34 -0700

    Clone Graph Problem

--- src/main/java/org/example/leetcode/CloneGraph.java
+++ src/main/java/org/example/leetcode/CloneGraph.java
@@ -2,9 +2,28 @@ package org.example.leetcode;
 
 import org.w3c.dom.Node;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CloneGraph {
 
-    public Node cloneGraph(Node node){
-
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
     }
+
+
+
 }
