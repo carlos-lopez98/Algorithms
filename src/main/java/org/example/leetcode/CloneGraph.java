b70418607ec3edf2925863c8f849467973cc1f6e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-03 13:51:10 -0700

    Clone Graph Review

--- src/main/java/org/example/leetcode/CloneGraph.java
+++ src/main/java/org/example/leetcode/CloneGraph.java
@@ -2,8 +2,7 @@ package org.example.leetcode;
 
 import org.w3c.dom.Node;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CloneGraph {
 
@@ -25,5 +24,37 @@ public class CloneGraph {
     }
 
 
+    //The intuition here is to create a clone of your starting node
+    //Then add that clone in as a value in your hashmap with the key being your actual node
+    //Then we iterate through each neighbor in the original node -
 
+
+    Map<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node){
+        //This is a recursive problem so we have base cases
+        if(node == null){
+            return null;
+        }
+
+        //This visited is used to store cloned copies of the original node
+        if(visited.containsKey(node)){
+            //Returns the copy of the node
+            return visited.get(node);
+        }
+
+        //We can't just add the neighbors in as a list
+        //That would create a reference copy
+        Node clone = new Node(node.val, new ArrayList<>());
+        //Ties the original to it's clone
+        visited.put(node, clone);
+
+        //Iterate through all the original neighbors
+        for(Node neighbor: node.neighbors){
+
+            //Add clones the the clone neighbors list
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clone;
+    }
 }
