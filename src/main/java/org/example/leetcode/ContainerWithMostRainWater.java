4ca279e863dba95d8948ed0ae9b9ff72da3ddb0c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-07 11:34:54 -0700

    Reviewing Container with Most Water

--- src/main/java/org/example/leetcode/ContainerWithMostRainWater.java
+++ src/main/java/org/example/leetcode/ContainerWithMostRainWater.java
@@ -23,18 +23,28 @@ public class ContainerWithMostRainWater {
         int leftWall = 0;
         int rightWall = height.length -1;
         int maxArea = 0;
+
+        //Two pointer approach - we move each wall inward with time
         while(leftWall < rightWall){
+            //At each iteration we calculate the area
+            //To avoid overflow we use math.min for the height
             int h = Math.min(height[leftWall], height[rightWall]);
             int w = Math.abs(rightWall - leftWall);
             int currArea = h * w;
+
+            //We move pointers based on which wall is greater
+            //move inward from right if left is greater
             if(height[leftWall] > height[rightWall]){
                 rightWall--;
+                //move left if rightwall is greater
             }else if (height[leftWall] < height[rightWall]){
                 leftWall++;
             }else{
+                //If they're equal we can move both this is common in many two pointer scenarios
                 leftWall++;
                 rightWall--;
             }
+
             maxArea = Math.max(currArea, maxArea);
         }
 
