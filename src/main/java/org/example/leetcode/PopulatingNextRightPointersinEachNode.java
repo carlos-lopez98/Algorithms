0908bd79fa76a33dcb2a8fa9864af589b0d06201
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-07 09:20:54 -0700

    Reviewing BFS

--- src/main/java/org/example/leetcode/PopulatingNextRightPointersinEachNode.java
+++ src/main/java/org/example/leetcode/PopulatingNextRightPointersinEachNode.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 public class PopulatingNextRightPointersinEachNode {
 
 
+//Node implentation will be provided - make sure to change this as variable names may be different
     class Node{
         int val;
         Node left;
@@ -31,26 +32,30 @@ public class PopulatingNextRightPointersinEachNode {
             return null;
         }
 
+        //Need our queue like usual in BFS
         Queue<Node> queue = new LinkedList<>();
         queue.add(root);
 
-        //BFS using two loops
-        //Outer while loop
-        //Inner for loop iterating through the children at the current level
+        //Iteration - while queue is not empty
         while(!queue.isEmpty()){
+
+            //Get's the size for our current level or breadth
             int nodesInLevel = queue.size();
 
+            //Iterates through our level
             for(int i = 0; i < nodesInLevel; i++){
                 Node curr = queue.poll();
 
+                //At the last element in our level
+                //We set next equal to null
                 if(i == nodesInLevel - 1){
                     curr.next = null;
                 }else{
+                    //if not we set next equal to the next element in our queue
+                    //ensure we don't pop() as that will break our algorithm
                     curr.next = queue.peek();
                 }
 
-                //Make sure we add left and then right children into our queue
-                //As that's the appropriate order
                 if(curr.left != null){
                     queue.add(curr.left);
                 }
