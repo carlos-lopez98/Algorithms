6c580d1559bd0a0877189b91a5c144b3c15aee6f
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-09 09:31:59 -0700

    House Robber Review

--- src/main/java/org/example/dynamicprogramming/HouseRobber.java
+++ src/main/java/org/example/dynamicprogramming/HouseRobber.java
@@ -7,7 +7,7 @@ public class HouseRobber {
     int[] housesWMoney;
     int[] cache;
 
-    public HouseRobber(int[] nums){
+    public HouseRobber(int[] nums) {
         this.housesWMoney = nums;
         this.cache = new int[nums.length];
 
@@ -15,23 +15,24 @@ public class HouseRobber {
         Arrays.fill(cache, -1);
     }
 
-    public int rob(int n){
-    if(n <= 0){
-        return housesWMoney[0];
-    }
+    public int rob(int n) {
+        //Base Case
+        if (n <= 0) {
+            return housesWMoney[0];
+        }
 
-    if(n == 1){
-        return Math.max(housesWMoney[0], housesWMoney[1]);
-    }
+        if (n == 1) {
+            return Math.max(housesWMoney[0], housesWMoney[1]);
+        }
 
-    //To cache - you need to recurse in on the cache
+        //To cache - you need to recurse in on the cache
         // This sets up every value in your cache
-    if(cache[n] != -1){
-        return cache[n];
-    }else{
-        cache[n] = Math.max(rob(n-1), housesWMoney[n] + rob(n-2));
-    }
+        if (cache[n] != -1) {
+            return cache[n];
+        } else {
+            cache[n] = Math.max(rob(n - 1), housesWMoney[n] + rob(n - 2));
+        }
 
-    return cache[n];
+        return cache[n];
     }
 }
