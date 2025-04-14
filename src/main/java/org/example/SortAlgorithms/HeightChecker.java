91bc20dd611482caa043abb8654113a81bbe0b6b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-14 09:54:37 -0700

    Sort Colors Solved

---
+++ src/main/java/org/example/SortAlgorithms/HeightChecker.java
@@ -0,0 +1,7 @@
+package org.example.SortAlgorithms;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+
+    }
+}
---
+++ src/main/java/org/example/SortAlgorithms/SortColors.java
@@ -0,0 +1,31 @@
+package org.example.SortAlgorithms;
+
+public class SortColors {
+
+    //This uses selection sort - O (n^2)
+    public void sortColors(int[] nums) {
+
+        for(int out = nums.length - 1; out > 0; out --){
+            int largestIndex = 0;
+
+            for(int inner = 0; inner <= out; inner++){
+                if(nums[inner] > nums[largestIndex]){
+                    largestIndex = inner;
+                }
+
+                if(inner == out){
+                    swap(inner, largestIndex, nums);
+                }
+            }
+        }
+
+    }
+
+
+    private void swap(int index, int maxIndex, int[] nums){
+        int temp = nums[index];
+
+        nums[index] = nums[maxIndex];
+        nums[maxIndex] = temp;
+    }
+}
