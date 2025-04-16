1c5cdf7e0810e014db1b2cfab958c929c10761cf
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-16 09:43:21 -0700

    FindMinInRotatedSortedArray Reviewed

--- src/main/java/org/example/leetcode/FindMinInRotatedSortedArray.java
+++ src/main/java/org/example/leetcode/FindMinInRotatedSortedArray.java
@@ -59,13 +59,15 @@ public class FindMinInRotatedSortedArray {
         while(left <= right){
             int midpoint = left + (right - left)/ 2;
 
+            //Remember bound checking - this is when you're at your minimum element
             if(midpoint > 0 && nums[midpoint] < nums[midpoint - 1]){
                 return nums[midpoint];
+
+                //This means our target is in the right side
             } else if (nums[midpoint] > nums[right]){
                 left = midpoint + 1;
             }else{
-                //Again you include right - since it can potentially be the element you need
-                right = midpoint;
+                right = midpoint - 1;
             }
         }
 
