6c52f300e980db3b0834e52ea89f1026b11dd1ce
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-17 09:41:21 -0700

    insertionSortList Review

--- src/main/java/org/example/sorting/InsertionSortList.java
+++ src/main/java/org/example/sorting/InsertionSortList.java
@@ -16,30 +16,22 @@ public class InsertionSortList {
 
         while(curr != null){
 
-            //In every iteration we need prev to reset back
-            //To the start of our sorted list
+            //Resets prev
             ListNode prev = dummy;
 
 
-            //We do this while loop to get to our insertion point
+            //Finds insertion point
             while(prev.next != null && prev.next.val <= curr.val){
                 prev = prev.next;
             }
 
 
-            //This saves our next point in our unsorted list
-            //That way we can continue the iteration afterward
+            //Splicing being done
             ListNode next =curr.next;
-
-            //This is our inserting first slice off prev.next and connect it to our
-            //Inserting element
             curr.next = prev.next;
-
-            //Next make our current element our inserted element
             prev.next = curr;
 
-            //Then move curr back to pointing to our unsorted list
-            //So that we can continue iterating through it
+            //Resets current
             curr = next;
         }
 
