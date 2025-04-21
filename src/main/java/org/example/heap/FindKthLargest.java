4be171a21ebce245b7866f2da2b477184db948db
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-21 09:04:59 -0700

    Reviewed findKthLargest

--- src/main/java/org/example/heap/FindKthLargest.java
+++ src/main/java/org/example/heap/FindKthLargest.java
@@ -6,7 +6,7 @@ import java.util.PriorityQueue;
 //Using a priority queue add all elements onto the queue
 //The original implementation of the priority queue will place elements
 //Into the queue in ascending fashion
-//Therfore when you have a queue larger than size k, you're popping a smaller element
+//Therefore when you have a queue larger than size k, you're popping a smaller element
 
 //At the end, you're left with a min heap exactly of length k - which the first element in the queue is
 //The kth largest element
@@ -15,17 +15,14 @@ public class FindKthLargest {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        //Add all elements to the priority queue
         for(int num: nums){
             minHeap.add(num);
 
-            //This ensures our kth largest becomes the first element in the resulting heap
             if(minHeap.size() > k){
                 minHeap.remove();
             }
         }
 
-        //Return the first element
         return minHeap.peek();
     }
 }
