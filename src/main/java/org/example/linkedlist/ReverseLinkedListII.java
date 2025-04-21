be45eda2147169b9489f8f7b6de6eaf46a820f6d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-21 09:02:57 -0700

    Reviewed ReverseLinkedListII

--- src/main/java/org/example/linkedlist/ReverseLinkedListII.java
+++ src/main/java/org/example/linkedlist/ReverseLinkedListII.java
@@ -16,33 +16,26 @@ public class ReverseLinkedListII {
 
     public void recurseAndReverse(ListNode right, int m, int n) {
 
-        //When n == 1 right pointer is in position
         if (n == 1) {
             return;
         }
 
         right = right.next;
 
-        //When m is less than one, we can stop moving left forwards as it's also now in position
         if (m > 1) {
             this.left = this.left.next;
         }
 
-        //Backtracking starts right after this call - remember that only our right is backtracking not our left
         this.recurseAndReverse(right, m - 1, n - 1);
 
-        //If pointers meet or right moves past left
+        //Stop pointer is needed, because we need to ensure that after right back tracks past left
+        //No further operations are performed
         if (this.left == right || right.next == this.left) {
-
-            //Then we stop
             this.stop = true;
         }
 
-        //If we're stopped then, we stop the swaps - backtracking will still continue - but no swaps will be performed
-        //Swaps only happen if our stop is still false
         if (!this.stop) {
 
-            //Then we swap values - and move our left pointer inward left = left.next
             int t = this.left.val;
             this.left.val = right.val;
             right.val = t;
