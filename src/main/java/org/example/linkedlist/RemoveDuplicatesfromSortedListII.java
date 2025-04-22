c80f45133dd552800181f15cdcd04dea6a33b0c6
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-22 08:38:34 -0700

    RemoveDuplicatesfromSortedListII Reviewed

--- src/main/java/org/example/linkedlist/RemoveDuplicatesfromSortedListII.java
+++ src/main/java/org/example/linkedlist/RemoveDuplicatesfromSortedListII.java
@@ -13,33 +13,29 @@ public class RemoveDuplicatesfromSortedListII {
 
             ListNode dummy = new ListNode(0, head);
 
-
-            //Technicall pred is also moving along the original linkedlist
-            //But it's one step before head
             ListNode pred = dummy;
 
             while (head != null) {
 
-                //You need the if statement because of the way you move pred
                 if (head.next != null && head.val == head.next.val) {
 
+                    //This skips all duplicates
                     while (head.next != null && head.val == head.next.val) {
                         head = head.next;
                     }
 
+                    //This skips the start of the duplicates
                     pred.next = head.next;
                 } else {
 
-                    //Otherwise we just move forward like normal, since the next element is unique
+                    //Only moves forward regularly if no dupes are involved
                     pred = pred.next;
                 }
 
-                // move forward
                 head = head.next;
             }
 
-            //Return the start position of the list
-            //This is common when working with linkedlist
+
             return dummy.next;
         }
 }
