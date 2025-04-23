3828663f317e8e66cd3c51ea32e904c31ca18f2d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-23 12:59:28 -0700

    PartitionLabels Reviewed

--- src/main/java/org/example/greedy/PartitionLabels.java
+++ src/main/java/org/example/greedy/PartitionLabels.java
@@ -7,22 +7,30 @@ import java.util.Map;
 
 public class PartitionLabels {
     public List<Integer> partitionLabels(String s) {
+
+        //create a hashmap to store each letters last occurrence
         Map<Character,Integer> lastOccurrence = new HashMap<>();
 
         for(int i = 0 ; i < s.length(); i++){
             lastOccurrence.put(s.charAt(i), i);
         }
 
+
         List<Integer> result = new ArrayList<>();
 
+        //You need a size to keep track of the current partition
         int size = 0;
+
+        //Every time your size reaches an end - it means you have a valid partition
         int end = 0;
 
         for(int i = 0; i < s.length(); i++){
             size++;
 
+            //End will update everytime to ensure we're staying within a valid partition
             end = Math.max(end, lastOccurrence.get(s.charAt(i)));
 
+            //reset size - at the new partition
             if(i == end){
                 result.add(size);
                 size = 0;
