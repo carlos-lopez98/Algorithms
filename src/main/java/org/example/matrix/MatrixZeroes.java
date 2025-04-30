0ca38374c668e09a215d6829f3c533921d11cad0
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-30 12:03:35 -0700

    Matrix Zeroes Reviewed

--- src/main/java/org/example/matrix/MatrixZeroes.java
+++ src/main/java/org/example/matrix/MatrixZeroes.java
@@ -8,8 +8,11 @@ public class MatrixZeroes {
         int R = matrix.length;
         int C = matrix[0].length;
 
+        //You can create a set to represent rows or columns
+        //Then you just add the row# or col# that is affected by a zero
         Set<Integer> rows = new HashSet<Integer>();
         Set<Integer> cols = new HashSet<Integer>();
+
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
@@ -20,6 +23,8 @@ public class MatrixZeroes {
             }
         }
 
+        //If your current row or column# is affected
+        //Change to zero
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 if (rows.contains(i) || cols.contains(j)) {
