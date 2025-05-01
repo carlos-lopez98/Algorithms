740cd46ea962a62937dfdd063ca63567b455ce22
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-30 21:05:57 -0700

    Setup ReverseLinkedListRecursion for review

--- src/main/java/org/example/linkedlist/ReverseLinkedList.java
+++ src/main/java/org/example/linkedlist/ReverseLinkedList.java
@@ -1,57 +1,10 @@
 package org.example.linkedlist;
 
 
+//Need: Outer variables outside of the recursion call stack
+//*When should our recursion stop? What would the base caes be?
+//When do we update our stop variable? Why's it needed?
 public class ReverseLinkedList {
 
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-
-    ListNode left;
-
-    public ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
-        }
-
-
-        this.left = head;
-
-        recurse(head);
-
-        return head;
-    }
-
-
-    public void recurse(ListNode head){
-        if(head == null){
-            return;
-        }
-
-        recurse(head.next);
-
-        if(this.left == head || head.next == left){
-            return;
-        }else{
-            int temp = this.left.val;
-            this.left.val = head.val;
-            head.val = temp;
-            this.left = left.next;
-        }
-    }
 
 }
