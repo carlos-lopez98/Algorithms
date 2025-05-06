c4d865cced97b29bbf75113e56ddb3705fe7512d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-05 19:45:18 -0700

    LemonadeChange Reviewed

--- src/main/java/org/example/greedy/LemonadeChange.java
+++ src/main/java/org/example/greedy/LemonadeChange.java
@@ -5,32 +5,35 @@ public class LemonadeChange {
     //Decision based algorithm
     //Guess it's greedy because we're tying to return the best possible answer for each bill in our bills
     public boolean lemonadeChange(int[] bills) {
-        int fiveDollarBill = 0;
-        int tenDollarBill = 0;
 
-        for(int bill : bills){
+        int fives = 0;
+        int tens = 0;
+
+        for(int bill: bills){
 
             if(bill == 5){
-                fiveDollarBill++;
+                fives++;
             }else if(bill == 10){
-                if(fiveDollarBill > 0){
-                    fiveDollarBill--;
-                    tenDollarBill++;
-                }else {
-                    return false;
-                }
 
-            }else {
-
-
-                if(tenDollarBill > 0 && fiveDollarBill > 0){
-                    tenDollarBill--;
-                    fiveDollarBill--;
-                }else if(fiveDollarBill > 2){
-                    fiveDollarBill -= 3;
+                if(fives > 0){
+                    fives--;
+                    tens++;
                 }else{
                     return false;
                 }
+
+
+            }else{
+
+                if(tens > 0 && fives > 0 ){
+                    tens--;
+                    fives--;
+                }else if(fives > 2){
+                    fives -= 3;
+                }else{
+                    return false;
+                }
+
             }
         }
 
