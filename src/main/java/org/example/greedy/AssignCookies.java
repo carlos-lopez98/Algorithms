ee4a834da432e50ecfbec07e3d75aabfbcd15e3b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-05 19:07:56 -0700

    Assign Cookies Reviewed

--- src/main/java/org/example/greedy/AssignCookies.java
+++ src/main/java/org/example/greedy/AssignCookies.java
@@ -6,25 +6,19 @@ public class AssignCookies {
 
     public int findContentChildren(int[] g, int[] s) {
 
-        //We must sort so we can try to give the smallest cookie to the smallest
-        //greed child
         Arrays.sort(g);
         Arrays.sort(s);
-        int cookieIndex = 0;
+
         int contentChildren = 0;
 
-        //Our bounds
-        while(cookieIndex < s.length && contentChildren < g.length){
-
-            //If our cookiew can satsify our child - then we have a content child
-            //Try the next child
-            if(s[cookieIndex] >= g[contentChildren]){
-                contentChildren++;
+        for(int i = 0; i < s.length; i++) {
+            if (contentChildren >= g.length) {
+                break;
             }
 
-            //If not we just keep trying a bigger cookie
-            //Only works because we sorted both arrays to start with
-            cookieIndex++;
+            if (s[i] >= g[contentChildren]) {
+                contentChildren++;
+            }
         }
 
         return contentChildren;
