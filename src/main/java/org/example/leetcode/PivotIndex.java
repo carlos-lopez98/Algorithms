1e6d88d156c1f79a82ce60f26d84af634cd382d4
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-07 18:26:37 -0700

    Pivot Index Reviewed

--- src/main/java/org/example/leetcode/PivotIndex.java
+++ src/main/java/org/example/leetcode/PivotIndex.java
@@ -10,20 +10,15 @@ public class PivotIndex {
 
         int sum = 0; int leftSum = 0;
 
-        //First you need your total sum
         for(int x: nums) sum+= x;
 
-        //Then you iterate keeping track of your prefixSum
         for(int i = 0; i < nums.length; i++){
 
-            //This is what determines what your pivotIndex is
-            //If your totalSum - current - prefix == prefix
-            //Then that means you're at your pivot
+
             if(leftSum == sum - nums[i] - leftSum){
                 return i;
             }
 
-            //Else move forward and update your prefix
             leftSum += nums[i];
         }
 
