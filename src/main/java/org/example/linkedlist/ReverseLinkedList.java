c42da78d79ff587261c00072d86269075cab428e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-13 13:33:14 -0700

    Reviewed ReverseLinkedList w Recursion

--- src/main/java/org/example/linkedlist/ReverseLinkedList.java
+++ src/main/java/org/example/linkedlist/ReverseLinkedList.java
@@ -6,5 +6,57 @@ package org.example.linkedlist;
 //When do we update our stop variable? Why's it needed?
 public class ReverseLinkedList {
 
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode left;
+    public boolean stop;
+
+    public ListNode reverseList(ListNode head) {
+
+        this.left = head;
+        this.stop = false;
+
+        helper(head);
+
+        return head;
+    }
+
+
+    public ListNode helper(ListNode head) {
+
+        if(head == null){
+            return null;
+        }
+
+        helper(head.next);
+
+        if(this.left == head || head.next == left){
+            stop = true;
+        }
+
+        if(!stop){
+            int temp = left.val;
+            left.val = head.val;
+            head.val = temp;
+            left = left.next;
+        }
+
+        return head;
+    }
 
 }
