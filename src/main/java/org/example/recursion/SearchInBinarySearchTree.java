cce1e77f199c710cfc73b5e304f7433ba862257d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-13 13:40:09 -0700

    Search in Binary Search Tree implemented using Recursion

--- src/main/java/org/example/recursion/SearchInBinarySearchTree.java
+++ src/main/java/org/example/recursion/SearchInBinarySearchTree.java
@@ -1,4 +1,41 @@
 package org.example.recursion;
 
 public class SearchInBinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val && root.right != null) {
+            return searchBST(root.right, val);
+        } else if (root.left != null) {
+            return searchBST(root.left, val);
+        }
+
+        return null;
+    }
 }
+
