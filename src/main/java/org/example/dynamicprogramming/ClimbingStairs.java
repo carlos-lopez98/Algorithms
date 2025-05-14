146eb4b01dd6fbadf8d3ed4de2c64b905a97975e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-14 10:10:10 -0700

    Reviewed Climbing Stairs using Memoization

--- src/main/java/org/example/dynamicprogramming/ClimbingStairs.java
+++ src/main/java/org/example/dynamicprogramming/ClimbingStairs.java
@@ -1,24 +1,7 @@
 package org.example.dynamicprogramming;
 
 public class ClimbingStairs {
+    public int climbStairs(int n) {
 
-    public int climbStairs(int n){
-    return dp(n);
-    }
-
-    private int dp(int i){
-        if (i <= 0){
-            return 0;
-        }
-
-        if(i == 1){
-            return 1;
-        }
-
-        if(i == 2){
-            return 2;
-        }
-
-        return dp(i - 1) + dp (i - 2);
     }
 }
