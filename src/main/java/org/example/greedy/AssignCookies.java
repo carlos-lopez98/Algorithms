2e859dd06fcd1d341c5d8ad6b183e24a7030ceb2
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-21 10:06:12 -0700

    Assign Cookies Reviewed

--- src/main/java/org/example/greedy/AssignCookies.java
+++ src/main/java/org/example/greedy/AssignCookies.java
@@ -2,6 +2,10 @@ package org.example.greedy;
 
 import java.util.Arrays;
 
+
+//The key here is to pick the array that you can continously walk through
+//In this case it's the cookies, once you have more contentChildren then children available
+//You can just return the content children
 public class AssignCookies {
 
     public int findContentChildren(int[] g, int[] s) {
