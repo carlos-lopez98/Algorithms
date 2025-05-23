1e3d8b75b22c00bfd45f9eae289acc423cf4cc8c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-23 12:24:01 -0700

    InsertionSortList Reviewed

--- src/main/java/org/example/sorting/InsertionSortList.java
+++ src/main/java/org/example/sorting/InsertionSortList.java
@@ -11,31 +11,26 @@ public class InsertionSortList {
 
 
   public ListNode insertionSortList(ListNode head) {
-        //You don't have to have an initial dummy.next
-        //Because in every iteration prev is adding to dummy.next
-        //so in the first iteration it'll be null -> prev -> prev -> prev
-        //Eventually dummy.next - is the head for a new sorted list
         ListNode dummy = new ListNode();
         ListNode curr =  head;
 
         while(curr != null){
 
-            //Resets prev
+            //Prev starts iterating everytime at dummy
             ListNode prev = dummy;
 
 
-            //Finds insertion point
             while(prev.next != null && prev.next.val <= curr.val){
                 prev = prev.next;
             }
 
 
-            //Splicing being done
+            //This is the splicing part
+            //We need to be able to visualize what's going on
+            //In order to do this properly and in the right order
             ListNode next =curr.next;
             curr.next = prev.next;
             prev.next = curr;
-
-            //Resets current
             curr = next;
         }
 
