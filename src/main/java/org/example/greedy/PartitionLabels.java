73d74b6d1496644298e6c68befa33e426f271189
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-05-22 17:17:58 -0700

    Reviewed PartitionLabels

--- src/main/java/org/example/greedy/PartitionLabels.java
+++ src/main/java/org/example/greedy/PartitionLabels.java
@@ -7,35 +7,25 @@ import java.util.Map;
 
 public class PartitionLabels {
     public List<Integer> partitionLabels(String s) {
+        //Scan string for each character's last occurrence
+        Map<Character, Integer> lastChar = new HashMap<>();
 
-        //create a hashmap to store each letters last occurrence
-        Map<Character,Integer> lastOccurrence = new HashMap<>();
-
-        for(int i = 0 ; i < s.length(); i++){
-            lastOccurrence.put(s.charAt(i), i);
+        for(int i = 0; i < s.length(); i++){
+            lastChar.put(s.charAt(i), i);
         }
 
-
-        List<Integer> result = new ArrayList<>();
-
-        //You need a size to keep track of the current partition
+        int end = 0;
         int size = 0;
 
-        //Every time your size reaches an end - it means you have a valid partition
-        int end = 0;
+        List<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < s.length(); i++){
             size++;
 
-            //The key is this little part
-            //The end of each partition is denoted by the last occurring element in our partition
-            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+            //Will set the end to the last possible character in our partition
+            //If we happen to be at that character we're at a partition
+            end = Math.max(end, lastChar.get(s.charAt(i)));
 
-            //whenever our I becomes our end
-            //It essentially means we reached the end of a valid partition
-            //Then we can reset the size
-            //end will reset in the next iteration as it has to become a new last occurring spot
-            //which logically will be greater than the current last occurring spot
             if(i == end){
                 result.add(size);
                 size = 0;
